dee97e6aebe0d52aa6880bae500f4351b3fbf110
Alex Grozav <alex.grozav@visma.com>
2018-03-12 10:44:12 +0200

    Updated radio button to extend radio.

--- src/components/RadioButton/script.js
+++ src/components/RadioButton/script.js
@@ -1,28 +1,6 @@
+import Radio from '../Radio';
+
 export default {
     name: 'RadioButton',
-    props: {
-        label: String,
-        value: String,
-        disabled: Boolean
-    },
-    computed: {
-        picked: {
-            get: function () {
-                return this.isGroup ? this.$parent.value : this.value;
-            },
-            set: function (val) {
-                if (this.isGroup) {
-                    this.$parent.$emit('childChange', val);
-                } else {
-                    this.$emit('input', val);
-                }
-            }
-        },
-        isGroup () {
-            return this.$parent.$options.name === 'RadioGroup';
-        },
-        isDisabled () {
-            return this.disabled;
-        }
-    }
+    extends: Radio
 };
