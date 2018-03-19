eb74b382ef4218ef798817467e8c5b710ab7e11f
Alex Grozav <alex@grozav.com>
2018-03-19 22:55:35 +0200

    Changed existing form components to use new mixins.

--- src/components/Checkbox/script.js
+++ src/components/Checkbox/script.js
@@ -1,51 +1,31 @@
+import BindableFormItem from '../../mixins/BindableFormItem';
+import DisableableFormItem from '../../mixins/DisableableFormItem';
+import FormItem from '../../mixins/FormItem';
+import GroupableFormItem from '../../mixins/GroupableFormitem';
+
 export default {
     name: 'Checkbox',
     props: {
         value: [Boolean, String],
-        disabled: Boolean
+        disabled: Boolean,
+        indeterminate: Boolean /* @TODO Implement indeterminate checkboxes */
     },
+    mixins: [
+        BindableFormItem,
+        DisableableFormItem,
+        FormItem,
+        GroupableFormItem
+    ],
     computed: {
-        /**
-         * Actual v-model used for the input
-         */
-        model: {
-            get: function () {
-                return this.isGroup ? this.parentGroup.value : this.value;
-            },
-            set: function (value) {
-                if (this.isGroup) {
-                    return this.parentGroup.$emit('change:input', value);
-                }
-
-                return this.$emit('input', value);
-            }
-        },
-        inputValue () {
-            return this.isGroup ? this.value : this.$attrs.value;
-        },
         classes () {
             return [
                 { '-disabled': this.isDisabled }
             ];
-        },
-        isGroup () {
-            let parent = this.$parent;
-
-            while (parent) {
-                if (parent.$options.name === 'CheckboxGroup') {
-                    this.parentGroup = parent;
-                    return true;
-                }
-
-                parent = parent.$parent;
-            }
-
-            return false;
-        },
-        isDisabled () {
-            return this.isGroup
-                ? this.parentGroup.disabled || this.disabled || (this.form || {}).disabled
-                : this.disabled || (this.form || {}).disabled;
+        }
+    },
+    mounted () {
+        if (this.indeterminate) {
+            this.$el.setAttribute('aria-controls', this.controls);
         }
     }
 };
--- src/components/Radio/script.js
+++ src/components/Radio/script.js
@@ -1,51 +1,25 @@
+import BindableFormItem from '../../mixins/BindableFormItem';
+import DisableableFormItem from '../../mixins/DisableableFormItem';
+import FormItem from '../../mixins/FormItem';
+import GroupableFormItem from '../../mixins/GroupableFormitem';
+
 export default {
     name: 'Radio',
     props: {
         value: String,
         disabled: Boolean
     },
+    mixins: [
+        BindableFormItem,
+        DisableableFormItem,
+        FormItem,
+        GroupableFormItem
+    ],
     computed: {
-        /**
-         * Actual v-model used for the input
-         */
-        model: {
-            get: function () {
-                return this.isGroup ? this.parentGroup.value : this.value;
-            },
-            set: function (value) {
-                if (this.isGroup) {
-                    return this.parentGroup.$emit('change:input', value);
-                }
-
-                return this.$emit('input', value);
-            }
-        },
-        inputValue () {
-            return this.isGroup ? this.value : this.$attrs.value;
-        },
         classes () {
             return [
                 { '-disabled': this.isDisabled }
             ];
-        },
-        isGroup () {
-            let parent = this.$parent;
-
-            while (parent) {
-                if (parent.$options.name === 'RadioGroup') {
-                    this.parentGroup = parent;
-                    return true;
-                }
-
-                parent = parent.$parent;
-            }
-
-            return false;
-        },
-        isDisabled () {
-            return this.isGroup
-                ? this.parentGroup.disabled || this.disabled || (this.form || {}).disabled
-                : this.disabled || (this.form || {}).disabled;
         }
     }
 };
