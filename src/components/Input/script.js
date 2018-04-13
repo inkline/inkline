cc16ec037f0a0161d3e268f581d2bff535e0584d
Alex Grozav <alex@grozav.com>
2018-04-13 12:10:01 +0300

    Fixed input binding and clearable #16

--- src/components/Input/script.js
+++ src/components/Input/script.js
@@ -2,12 +2,14 @@ import InputGroup from '../InputGroup';
 
 import BindableIndividualFormItem from '../../mixins/forms/BindableIndividualFormItem';
 import ClearableFormItem from '../../mixins/forms/ClearableFormItem';
+import ClickableFormItem from '../../mixins/forms/ClickableFormItem';
 import DisableableFormItem from '../../mixins/forms/DisableableFormitem';
 import FocusableFormItem from '../../mixins/forms/FocusableFormItem';
 import GroupableFormItem from '../../mixins/forms/GroupableFormitem';
 import InjectableFormItem from '../../mixins/forms/InjectableFormItem';
 import InputableFormItem from '../../mixins/forms/InputableFormItem';
 import LabelableFormItem from '../../mixins/forms/LabelableFormItem';
+import TabableFormItem from '../../mixins/forms/TabableFormItem';
 
 import ChangeableComponent from '../../mixins/components/ChangeableComponent';
 import ClassableComponent from '../../mixins/components/ClassableComponent';
@@ -27,12 +29,14 @@ export default {
     mixins: [
         BindableIndividualFormItem,
         ClearableFormItem,
+        ClickableFormItem,
         DisableableFormItem,
         FocusableFormItem,
         GroupableFormItem,
         InjectableFormItem,
         InputableFormItem,
         LabelableFormItem,
+        TabableFormItem,
 
         ChangeableComponent,
         ClassableComponent,
