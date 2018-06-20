6f299f8e8dedcae0ffcc608e9de78e2f3246c892
Alex Grozav <alex@grozav.com>
2018-06-21 00:11:30 +0300

    Updated button and added attributable mixin.

---
+++ src/mixins/components/AttributableComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data () {
+        return {
+            attributesProvider: {}
+        };
+    },
+    watch: {
+        attributesProvider: {
+            handler (value, oldValue) {
+                console.log('CHANGE', value)
+                Object.keys(this.attributesProvider).forEach((key) => {
+                    if (!this.attributesProvider.hasOwnProperty(key)) { return; }
+
+                    this.$attrs[key] = this.attributesProvider[key]();
+
+                    // attributeRules[key] = this.attributesProvider[key].map((attributeRule) => attributeRule());
+                    // attributeRules['*'] = attributeRules['*'].concat(attributeRules[key]);
+                });
+            },
+            deep: true
+        }
+    }
+};
+</script>
