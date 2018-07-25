efbc77c2a12c0ea010f38c8ec19166b683edb2f7
Alex Grozav <alex@grozav.com>
2018-07-25 16:20:19 +0300

    Added proper checked method for checkboxes.

--- src/components/Checkbox/script.js
+++ src/components/Checkbox/script.js
@@ -9,6 +9,17 @@ export default {
         value: [Boolean, String],
         indeterminate: Boolean /* @TODO Implement indeterminate checkboxes */
     },
+    computed: {
+        checked () {
+            if ({}.toString.call(this.model) === '[object Boolean]') {
+                return this.model;
+            } else if (Array.isArray(this.model)) {
+                return this.model.indexOf(this.currentValue) !== -1;
+            } else if (this.model !== null && this.model !== undefined) {
+                return this.model === this.currentValue;
+            }
+        }
+    },
     mounted () {
         if (this.indeterminate) {
             this.$el.setAttribute('aria-controls', this.controls);
