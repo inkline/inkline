c453a662e0e6c666a219286f8c1687e11fccbea8
Alex Grozav <alex@grozav.com>
2018-10-04 22:05:01 +0300

    Added active property mixin.

---
+++ src/mixins/components/properties/ActivePropertyMixin.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        active: {
+            type: Boolean,
+            default: false
+        },
+        activeClass: {
+            type: String,
+            default: '-active'
+        }
+    },
+    created () {
+        if (this.classesProvider) {
+            this.classesProvider['root'].push(() => ({
+                '-active': this.active ? this.activeClass : false
+            }));
+        }
+    }
+};
+</script>
