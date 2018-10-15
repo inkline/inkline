58f8353ea7b28612bf2c9c46a04db90ff952dd60
Alex Grozav <alex@grozav.com>
2018-10-16 00:25:06 +0300

    Added collapsible item component.

---
+++ src/components/CollapsibleItem/script.js
@@ -0,0 +1,46 @@
+import { uid } from '../../helpers/unique-id';
+
+import TransitionExpand from '../../transitions/TransitionExpand';
+
+import AttributesProviderMixin from '../../mixins/components/providers/AttributesProviderMixin';
+import ClassesProviderMixin from '../../mixins/components/providers/ClassesProviderMixin';
+import EmitProviderMixin from '../../mixins/components/providers/EmitProviderMixin';
+
+import ActiveClassPropertyMixin from '../../mixins/components/properties/ActiveClassPropertyMixin';
+
+export default {
+    name: 'Collapsible',
+    mixins: [
+        AttributesProviderMixin,
+        ClassesProviderMixin,
+        EmitProviderMixin,
+
+        ActiveClassPropertyMixin,
+    ],
+    components: {
+        TransitionExpand
+    },
+    inject: ['collapsible'],
+    props: {
+        title: {
+            type: String,
+            default: ''
+        },
+        id: {
+            type: String,
+            default () {
+                return uid('collapsible-item');
+            }
+        }
+    },
+    computed: {
+        active() {
+            return this.collapsible.active.indexOf(this.id) > -1;
+        },
+    },
+    methods: {
+        onClick() {
+            this.dispatch('Collapsible', 'item-click', this);
+        },
+    }
+};
