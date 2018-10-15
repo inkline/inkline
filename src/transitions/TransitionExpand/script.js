841078297928df551b9aa8ce27cfee0d66b51c6b
Alex Grozav <alex@grozav.com>
2018-10-16 00:26:59 +0300

    Added transition component for collapsibles

---
+++ src/transitions/TransitionExpand/script.js
@@ -0,0 +1,44 @@
+export default {
+    name: 'TransitionExpand',
+    functional: true,
+
+    render(h, context) {
+        const data = {
+            props: {
+                name: 'expand',
+            },
+            on: {
+                enter(element) {
+                    const width = getComputedStyle(element).width;
+                    element.style.width = width;
+                    element.style.position = 'absolute';
+                    element.style.visibility = 'hidden';
+                    element.style.height = 'auto';
+
+                    const height = getComputedStyle(element).height;
+                    element.style.width = null;
+                    element.style.position = null;
+                    element.style.visibility = null;
+                    element.style.height = 0;
+
+                    setTimeout(() => {
+                        element.style.height = height;
+                    });
+                },
+                afterEnter(element) {
+                    element.style.height = 'auto';
+                },
+                leave(element) {
+                    const height = getComputedStyle(element).height;
+                    element.style.height = height;
+
+                    setTimeout(() => {
+                        element.style.height = 0;
+                    });
+                }
+            }
+        };
+
+        return h('transition', data, context.children);
+    }
+};
