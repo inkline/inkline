12ba79a69c714c6e8ad7ab07eecb9025ba2d07e1
Alex Grozav <alex@grozav.com>
2018-10-31 08:22:03 +0200

    Fixed transition expand.

--- src/transitions/TransitionExpand/script.js
+++ src/transitions/TransitionExpand/script.js
@@ -9,14 +9,11 @@ export default {
             },
             on: {
                 enter(element) {
-                    const width = getComputedStyle(element).width;
-                    element.style.width = width;
                     element.style.position = 'absolute';
                     element.style.visibility = 'hidden';
                     element.style.height = 'auto';
 
                     const height = getComputedStyle(element).height;
-                    element.style.width = null;
                     element.style.position = null;
                     element.style.visibility = null;
                     element.style.height = 0;
