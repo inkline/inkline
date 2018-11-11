acf6dcf4a5d1f1554945d360b060747eee16c7a6
Alex Grozav <alex@grozav.com>
2018-11-11 16:55:16 +0200

    Added event helpers: on, of, once.

---
+++ src/helpers/off.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+export const off = (function() {
+    if (!Vue.prototype.$isServer && document.removeEventListener) {
+        return function(element, event, handler) {
+            if (element && event) {
+                element.removeEventListener(event, handler, false);
+            }
+        };
+    } else {
+        return function(element, event, handler) {
+            if (element && event) {
+                element.detachEvent('on' + event, handler);
+            }
+        };
+    }
+})();
---
+++ src/helpers/on.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+export const on = (function() {
+    if (!Vue.prototype.$isServer && document.addEventListener) {
+        return function(element, event, handler) {
+            if (element && event && handler) {
+                element.addEventListener(event, handler, false);
+            }
+        };
+    } else {
+        return function(element, event, handler) {
+            if (element && event && handler) {
+                element.attachEvent('on' + event, handler);
+            }
+        };
+    }
+})();
