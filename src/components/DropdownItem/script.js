53757b58a7ecb483b7686e7013aeb947882b21c4
Alex Grozav <alex@grozav.com>
2018-11-12 21:43:10 +0200

    Added dropdown item.

---
+++ src/components/DropdownItem/script.js
@@ -0,0 +1,37 @@
+import DisabledPropertyMixin from 'inkline/mixins/components/properties/DisabledPropertyMixin';
+
+import ClassesProviderMixin from 'inkline/mixins/components/providers/ClassesProviderMixin';
+import EmitProviderMixin from 'inkline/mixins/components/providers/EmitProviderMixin';
+
+export default {
+    name: 'IDropdownItem',
+    mixins: [
+        DisabledPropertyMixin,
+
+        ClassesProviderMixin,
+        EmitProviderMixin
+    ],
+    props: {
+        divider: {
+            type: Boolean,
+            default: true
+        },
+    },
+    methods: {
+        onClick() {
+            this.dispatch('IDropdown', 'menu-item-click', [this.command, this]);
+        }
+    },
+    computed: {
+        tabindex() {
+            return this.disabled ? null : -1;
+        }
+    },
+    created () {
+        if (this.classesProvider) {
+            this.classesProvider['root'].push(() => ({
+                '-divider': this.divider
+            }));
+        }
+    }
+};
