7d516fddde9e706ecf32b60bd63f002cb81932cd
Alex Grozav <alex@grozav.com>
2018-12-17 14:15:42 +0200

    Added popup controls provider tests.

--- tests/unit/mixins/components/providers/PopupControlsProviderMixin.spec.js
+++ tests/unit/mixins/components/providers/PopupControlsProviderMixin.spec.js
@@ -86,6 +86,110 @@ describe('Mixins', () => {
         });
 
         describe('methods', () => {
+            describe('onClick()', () => {
+                it('should call hide() if visible', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'hide');
+                    wrapper.setData({ visible: true });
+
+                    wrapper.vm.onClick();
+
+                    expect(spy).toHaveBeenCalled();
+                });
+
+                it('should call show() if not visible', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'show');
+                    wrapper.setData({ visible: false });
+
+                    wrapper.vm.onClick();
+
+                    expect(spy).toHaveBeenCalled();
+                });
+
+                it('should not execute if disabled', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'hide');
+                    wrapper.setData({ visible: true });
+                    wrapper.setProps({ disabled: true });
+
+                    wrapper.vm.onClick();
+
+                    expect(spy).not.toHaveBeenCalled();
+                });
+            });
+
+            describe('show()', () => {
+                it('should set visible to true instantly if trigger is set to "click"', (done) => {
+                    wrapper.setData({ visible: false });
+
+                    wrapper.vm.show();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(true);
+                        done();
+                    }, 0);
+                });
+
+                it('should set visible to true after showTimeout if trigger is set to "hover"', (done) => {
+                    wrapper.setData({ visible: false });
+                    wrapper.setProps({ trigger: 'hover' });
+
+                    wrapper.vm.show();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(true);
+                        done();
+                    }, 500);
+                });
+
+                it('should not set visible to true if disabled', (done) => {
+                    wrapper.setData({ visible: false });
+                    wrapper.setProps({ disabled: true });
+
+                    wrapper.vm.show();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(false);
+                        done();
+                    }, 0);
+                });
+            });
+
+            describe('hide()', () => {
+                it('should set visible to false instantly if trigger is set to "click"', (done) => {
+                    wrapper.setData({ visible: true });
+
+                    wrapper.vm.hide();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(false);
+                        done();
+                    }, 0);
+                });
+
+                it('should set visible to false after showTimeout if trigger is set to "hover"', (done) => {
+                    wrapper.setData({ visible: true });
+                    wrapper.setProps({ trigger: 'hover' });
+
+                    wrapper.vm.hide();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(false);
+                        done();
+                    }, 500);
+                });
+
+                it('should not set visible to false if disabled', (done) => {
+                    wrapper.setData({ visible: true });
+                    wrapper.setProps({ disabled: true });
+
+                    wrapper.vm.hide();
+
+                    setTimeout(() => {
+                        expect(wrapper.vm.visible).toEqual(true);
+                        done();
+                    }, 0);
+                });
+            });
+
             describe('initElements()', () => {
                 it('should throw error if trigger element not provided', () => {
                     wrapper.vm.$slots.default = [];
@@ -95,12 +199,12 @@ describe('Mixins', () => {
                 });
 
                 it('should set triggerElement and dropdownElement', () => {
-                    wrapper.setData({ triggerElement: null, dropdownElement: null });
+                    wrapper.setData({ triggerElement: null, popupElement: null });
 
                     wrapper.vm.initElements();
 
                     expect(wrapper.vm.triggerElement).not.toEqual(null);
-                    expect(wrapper.vm.dropdownElement).not.toEqual(null);
+                    expect(wrapper.vm.popupElement).not.toEqual(null);
                 });
             });
 
