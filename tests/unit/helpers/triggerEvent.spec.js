bbaae56cbffabc3f3502473e4ab87bf51cc85b33
Alex Grozav <alex@grozav.com>
2018-12-22 12:24:12 +0200

    Finished adding helper tests.

---
+++ tests/unit/helpers/triggerEvent.spec.js
@@ -0,0 +1,110 @@
+import { triggerEvent } from 'inkline/helpers/triggerEvent';
+
+describe('Helpers', () => {
+    describe('triggerEvent()', () => {
+        let element;
+
+        beforeEach(() => {
+            element = document.createElement('a');
+        });
+
+        describe('dispatchEvent', () => {
+            it('should trigger an event of type MouseEvents', () => {
+                const eventName = 'mousemove';
+                const eventType = 'MouseEvents';
+                const spy = jest.spyOn(element, 'dispatchEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith(event);
+            });
+
+            it('should trigger an event of type KeyboardEvent', () => {
+                const eventName = 'keypress';
+                const eventType = 'KeyboardEvent';
+                const spy = jest.spyOn(element, 'dispatchEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith(event);
+            });
+
+            it('should trigger an event of type HTMLEvents', () => {
+                const eventName = 'other';
+                const eventType = 'HTMLEvents';
+                const spy = jest.spyOn(element, 'dispatchEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith(event);
+            });
+        });
+
+        describe('fireEvent', () => {
+            beforeEach(() => {
+                element.dispatchEvent = false;
+                element.fireEvent = () => {};
+            });
+
+            it('should trigger an event of type MouseEvents', () => {
+                const eventName = 'mousemove';
+                const eventType = 'MouseEvents';
+                const spy = jest.spyOn(element, 'fireEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith('on' + eventName, event);
+            });
+
+            it('should trigger an event of type KeyboardEvent', () => {
+                const eventName = 'keypress';
+                const eventType = 'KeyboardEvent';
+                const spy = jest.spyOn(element, 'fireEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith('on' + eventName, event);
+            });
+
+            it('should trigger an event of type HTMLEvents', () => {
+                const eventName = 'other';
+                const eventType = 'HTMLEvents';
+                const spy = jest.spyOn(element, 'fireEvent');
+
+                const event = document.createEvent(eventType);
+                event.initEvent(eventName);
+
+                triggerEvent(element, eventName);
+
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith('on' + eventName, event);
+            });
+        });
+    });
+});
---
+++ tests/unit/helpers/uid.spec.js
@@ -0,0 +1,13 @@
+import { uid } from 'inkline/helpers/uid';
+
+describe('Helpers', () => {
+    describe('uid()', () => {
+        it('should return an unique string starting with baseId', () => {
+            expect(uid('example')).toEqual(expect.stringContaining('example'));
+        });
+
+        it('should return a different string every call', () => {
+            expect(uid('any')).not.toEqual(uid('any'));
+        });
+    });
+});
