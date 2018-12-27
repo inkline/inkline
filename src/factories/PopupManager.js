608e868bdbe292df2eb4fa25a915dad463897e85
Alex Grozav <alex@grozav.com>
2018-12-27 20:07:27 +0200

    Updated popup manager functionality. Added tests.

--- src/factories/PopupManager.js
+++ src/factories/PopupManager.js
@@ -1,34 +1,24 @@
 import Vue from 'vue';
-import { addClass, removeClass, isKey } from 'inkline/helpers/index';
+import { isKey } from 'inkline/helpers/index';
 
-class PopupManager {
+export class PopupManager {
     instances = {};
-
     modalStack = [];
-    modalFade = true;
-
-    hasModal = false;
     zIndex = 2000;
-    
+
     constructor() {
         if (!Vue.prototype.$isServer) {
-            // handle `esc` key when the popup is shown
+            // Handle `esc` key when the popup is shown
             window.addEventListener('keydown', (e) => {
                 if (isKey('esc', e)) {
                     const topPopup = this.getTopPopup();
 
                     if (topPopup && topPopup.closeOnPressEscape) {
-                        topPopup.handleClose
-                            ? topPopup.handleClose()
-                            : (topPopup.handleAction ? topPopup.handleAction('cancel') : topPopup.close());
+                        topPopup.close();
                     }
                 }
             });
         }
-    }
-
-    getInstance(id) {
-        return this.instances[id];
     }
 
     register(instance) {
@@ -37,10 +27,10 @@ class PopupManager {
         }
     }
 
-    unregister(id) {
-        if (id) {
-            this.instances[id] = null;
-            delete this.instances[id];
+    unregister(instance) {
+        if (instance && instance.id) {
+            this.instances[instance.id] = null;
+            delete this.instances[instance.id];
         }
     }
 
@@ -48,146 +38,33 @@ class PopupManager {
         return this.zIndex++;
     }
 
-    doOnModalClick() {
-        const topItem = this.modalStack[this.modalStack.length - 1];
-
-        if (!topItem) return;
-
-        const instance = this.getInstance(topItem.id);
-        if (instance && instance.closeOnClickModal) {
-            instance.close();
-        }
-    }
-
-    getModal() {
-        if (Vue.prototype.$isServer) {
-            return;
-        }
-
-        let modalDom = this.modalDom;
-
-        if (modalDom) {
-            this.hasModal = true;
-        } else {
-            this.hasModal = false;
-            modalDom = document.createElement('div');
-            this.modalDom = modalDom;
-
-            modalDom.addEventListener('touchmove', (event) => {
-                event.preventDefault();
-                event.stopPropagation();
-            });
-
-            modalDom.addEventListener('click', () => {
-                this.doOnModalClick && this.doOnModalClick();
-            });
-        }
-
-        return modalDom;
-    }
-
-    openModal(id, zIndex, dom, modalClass, modalFade) {
+    openModal(id) {
         if (Vue.prototype.$isServer) return;
-        if (!id || zIndex === undefined) return;
-        this.modalFade = modalFade;
 
-        const modalStack = this.modalStack;
+        const modal = this.modalStack.find((m) => m.id === id);
 
-        for (let i = 0, j = modalStack.length; i < j; i++) {
-            const item = modalStack[i];
-            if (item.id === id) {
-                return;
-            }
-        }
-
-        const modalDom = this.getModal();
-
-        addClass(modalDom, 'v-modal');
-
-        if (this.modalFade && !this.hasModal) {
-            addClass(modalDom, 'v-modal-enter');
-        }
-
-        if (modalClass) {
-            let classArr = modalClass.trim().split(/\s+/);
-            classArr.forEach(item => addClass(modalDom, item));
-        }
-
-        setTimeout(() => {
-            removeClass(modalDom, 'v-modal-enter');
-        }, 200);
-
-        if (dom && dom.parentNode && dom.parentNode.nodeType !== 11) {
-            dom.parentNode.appendChild(modalDom);
+        if (modal) {
+            modal.$el.style.zIndex = this.nextZIndex();
         } else {
-            document.body.appendChild(modalDom);
+            this.modalStack.push({ id: id });
         }
-
-        if (zIndex) {
-            modalDom.style.zIndex = zIndex;
-        }
-
-        modalDom.tabIndex = 0;
-        modalDom.style.display = '';
-
-        this.modalStack.push({ id: id, zIndex: zIndex, modalClass: modalClass });
     }
 
     closeModal(id) {
-        const modalStack = this.modalStack;
-        const modalDom = this.getModal();
+        const modalIndex = this.modalStack.findIndex((m) => m.id === id);
 
-        if (modalStack.length > 0) {
-            const topItem = modalStack[modalStack.length - 1];
-
-            if (topItem.id === id) {
-                if (topItem.modalClass) {
-                    let classArr = topItem.modalClass.trim().split(/\s+/);
-                    classArr.forEach(item => removeClass(modalDom, item));
-                }
-
-                modalStack.pop();
-                if (modalStack.length > 0) {
-                    modalDom.style.zIndex = modalStack[modalStack.length - 1].zIndex;
-                }
-            } else {
-                for (let i = modalStack.length - 1; i >= 0; i--) {
-                    if (modalStack[i].id === id) {
-                        modalStack.splice(i, 1);
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (modalStack.length === 0) {
-            if (this.modalFade) {
-                addClass(modalDom, 'v-modal-leave');
-            }
-
-            setTimeout(() => {
-                if (modalStack.length === 0) {
-                    if (modalDom.parentNode) {
-                        modalDom.parentNode.removeChild(modalDom);
-                    }
-                    modalDom.style.display = 'none';
-
-                    this.modalDom = undefined;
-                }
-
-                removeClass(modalDom, 'v-modal-leave');
-            }, 200);
-        }
+        this.modalStack.splice(modalIndex, 1);
     }
 
     getTopPopup() {
         if (Vue.prototype.$isServer) return;
+
         if (this.modalStack.length > 0) {
             const topPopup = this.modalStack[this.modalStack.length - 1];
 
             if (!topPopup) return;
 
-            return this.getInstance(topPopup.id);
+            return this.instances[topPopup.id];
         }
     }
 }
--- tests/unit/factories/PopupManager.spec.js
+++ tests/unit/factories/PopupManager.spec.js
@@ -1,75 +1,190 @@
-import ClickOutside from 'inkline/directives/click-outside';
+import { triggerEvent } from 'inkline/helpers/triggerEvent';
+import { PopupManager } from 'inkline/factories/PopupManager';
 
-describe('Directives', () => {
-    describe('v-click-outside', () => {
-        let element;
-        let binding;
-        let vnode;
+describe('Factories', () => {
+    describe('PopupManager', () => {
+        let popupManager;
 
         beforeEach(() => {
-            element = { contains: () => false };
-            binding = { expression: 'fn', value: () => {} };
-            vnode = { context: { fn: () => {} } };
+            popupManager = new PopupManager();
         });
 
-        describe('createDocumentHandler()', () => {
-            it('should return if vnode not provided', () => {
-                ClickOutside.bind(element, binding, undefined);
-
-                expect(element['@@clickOutsideContext'].documentHandler()).toEqual(undefined);
+        describe('props', () => {
+            describe('instances', () => {
+                it('should be defined', () => {
+                    expect(popupManager.instances).toBeDefined();
+                    expect(popupManager.instances).toEqual({});
+                });
             });
 
-            it('should call binding fn from vnode', () => {
-                ClickOutside.bind(element, binding, vnode);
-                const spy = jest.spyOn(vnode.context, 'fn');
+            describe('modalStack', () => {
+                it('should be defined', () => {
+                    expect(popupManager.modalStack).toBeDefined();
+                    expect(popupManager.modalStack).toEqual([]);
+                });
+            });
 
-                element['@@clickOutsideContext'].documentHandler({ target: {} }, { target: {} });
+            describe('zIndex', () => {
+                it('should be defined', () => {
+                    expect(popupManager.zIndex).toBeDefined();
+                    expect(popupManager.zIndex).toEqual(2000);
+                });
+            });
+        });
+
+        describe('constructor()', () => {
+            it('should add keydown event listener', () => {
+                const spy = jest.spyOn(window, 'addEventListener');
+
+                popupManager = new PopupManager();
+
+                expect(spy).toHaveBeenCalled();
+                expect(spy).toHaveBeenCalledWith('keydown', expect.any(Function));
+            });
+
+            it('should close topmost modal on pressing Escape', () => {
+                const instance = { id: 'abc', close: () => {}, closeOnPressEscape: true };
+                const spy = jest.spyOn(instance, 'close');
+
+                popupManager.register(instance);
+                popupManager.openModal('abc');
+
+                const event = new KeyboardEvent('keydown', {
+                    key: 'Escape'
+                });
+                window.dispatchEvent(event);
+
                 expect(spy).toHaveBeenCalled();
             });
 
-            it('should call binding fn from from element', () => {
-                vnode.context.fn = false;
+            it('should not close topmost modal on pressing Escape if closeOnPressEscape is false', () => {
+                const instance = { id: 'abc', close: () => {}, closeOnPressEscape: false };
+                const spy = jest.spyOn(instance, 'close');
 
-                ClickOutside.bind(element, binding, vnode);
-                const spy = jest.spyOn(element['@@clickOutsideContext'], 'bindingFn');
+                popupManager.register(instance);
+                popupManager.openModal('abc');
 
-                element['@@clickOutsideContext'].documentHandler({ target: {} }, { target: {} });
-                expect(spy).toHaveBeenCalled();
+                const event = new KeyboardEvent('keydown', {
+                    key: 'Escape'
+                });
+                window.dispatchEvent(event);
+
+                expect(spy).not.toHaveBeenCalled();
             });
         });
 
-        describe('bind()', () => {
-            it('should add element click-outside handler', () => {
-                ClickOutside.bind(element, binding, vnode);
+        describe('register()', () => {
+            it('should register modal instance', () => {
+                const instance = { id: 'abc' };
 
-                expect(element['@@clickOutsideContext'].id).toEqual(expect.any(Number));
-                expect(element['@@clickOutsideContext'].documentHandler).toEqual(expect.any(Function));
-                expect(element['@@clickOutsideContext'].methodName).toEqual(binding.expression);
-                expect(element['@@clickOutsideContext'].bindingFn).toEqual(binding.value);
+                popupManager.register(instance);
+
+                expect(popupManager.instances).toHaveProperty(instance.id);
+                expect(popupManager.instances[instance.id]).toEqual(instance);
+            });
+
+            it('should not register modal instance if instance not defined', () => {
+                popupManager.register(null);
+
+                expect(Object.keys(popupManager.instances).length).toEqual(0);
+            });
+
+            it('should not register modal instance if id not present', () => {
+                const instance = {};
+
+                popupManager.register(instance);
+
+                expect(Object.keys(popupManager.instances).length).toEqual(0);
             });
         });
 
-        describe('update()', () => {
-            it('should update element click-outside handler', () => {
-                ClickOutside.bind(element, binding, vnode);
+        describe('unregister()', () => {
+            it('should unregister modal instance', () => {
+                const instance = { id: 'abc' };
 
-                binding.expression = 'new';
-                binding.value = false;
+                popupManager.register(instance);
+                popupManager.unregister(instance);
 
-                ClickOutside.update(element, binding, vnode);
+                expect(popupManager.instances).not.toHaveProperty(instance.id);
+                expect(popupManager.instances[instance.id]).not.toBeDefined();
+            });
 
-                expect(element['@@clickOutsideContext'].documentHandler).toEqual(expect.any(Function));
-                expect(element['@@clickOutsideContext'].methodName).toEqual(binding.expression);
-                expect(element['@@clickOutsideContext'].bindingFn).toEqual(binding.value);
+            it('should not unregister modal instance if id not present', () => {
+                const instance = { id: 'abc' };
+
+                popupManager.register(instance);
+                popupManager.unregister({});
+
+                expect(Object.keys(popupManager.instances).length).toEqual(1);
+            });
+
+            it('should not unregister modal instance if id not present', () => {
+                const instance = { id: 'abc' };
+
+                popupManager.register(instance);
+                popupManager.unregister(null);
+
+                expect(Object.keys(popupManager.instances).length).toEqual(1);
             });
         });
 
-        describe('unbind()', () => {
-            it('should delete element click-outside handler', () => {
-                ClickOutside.bind(element, binding, vnode);
-                ClickOutside.unbind(element);
+        describe('nextZIndex()', () => {
+            it('should return incremented zIndex', () => {
+                const zIndex = popupManager.zIndex;
 
-                expect(element['@@clickOutsideContext']).not.toBeDefined();
+                expect(popupManager.nextZIndex()).toEqual(zIndex);
+                expect(popupManager.nextZIndex()).toEqual(zIndex + 1);
+                expect(popupManager.nextZIndex()).toEqual(zIndex + 2);
+            });
+        });
+
+        describe('openModal()', () => {
+            it('should add modal to modal stack if not already open', () => {
+                popupManager.openModal('abc');
+
+                expect(popupManager.modalStack.length).toEqual(1);
+                expect(popupManager.modalStack[0]).toEqual({ id: 'abc' });
+            });
+
+            it('should set modal zIndex if already open', () => {
+                popupManager.modalStack.push({
+                    id: 'abc',
+                    $el: {
+                        style: {}
+                    }
+                });
+
+                popupManager.openModal('abc');
+
+                expect(popupManager.modalStack.length).toEqual(1);
+                expect(popupManager.modalStack[0].$el.style.zIndex).toEqual(2000);
+            });
+        });
+
+        describe('closeModal()', () => {
+            it('should remove modal from modal stack', () => {
+                popupManager.openModal('abc');
+                popupManager.closeModal('abc');
+
+                expect(popupManager.modalStack.length).toEqual(0);
+            });
+        });
+
+        describe('getTopPopup()', () => {
+            it('should return instance of topmost open modal', () => {
+                popupManager.register({ id: 'abc' });
+                popupManager.register({ id: 'def' });
+
+                popupManager.openModal('abc');
+                popupManager.openModal('def');
+
+                expect(popupManager.getTopPopup()).toEqual({ id: 'def' });
+            });
+
+            it('should return undefined if no open modals', () => {
+                popupManager.register({ id: 'abc' });
+
+                expect(popupManager.getTopPopup()).not.toBeDefined();
             });
         });
     });
