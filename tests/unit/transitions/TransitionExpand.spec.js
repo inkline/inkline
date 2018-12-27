ee630075b4c50184f336e2d5218a83b3b6518868
Alex Grozav <alex@grozav.com>
2018-12-27 20:08:25 +0200

    Added transition expand tests. (in progress)

--- tests/unit/transitions/TransitionExpand.spec.js
+++ tests/unit/transitions/TransitionExpand.spec.js
@@ -1,79 +1,34 @@
 import { mount } from '@vue/test-utils'
 
-import AttributesProviderMixin from 'inkline/mixins/components/providers/AttributesProviderMixin';
+import TransitionExpand from 'inkline/transitions/TransitionExpand';
 
-describe('Mixins', () => {
-    describe('AttributesProviderMixin', () => {
+describe('Transitions', () => {
+    describe('TransitionExpand', () => {
         let wrapper;
 
         beforeEach(() => {
-            wrapper = mount({
-                render () {},
-                mixins: [
-                    AttributesProviderMixin,
-                ]
-            });
+            wrapper = mount(TransitionExpand);
         });
 
-        describe('data', () => {
-            describe('attributesProvider', () => {
-                it('should be defined', () => {
-                    expect(wrapper.vm.attributesProvider).toBeDefined();
-                    expect(wrapper.vm.attributesProvider.constructor).toEqual(Array);
-                });
-            });
+        it('should be named correctly', () => {
+           expect(TransitionExpand.name).toEqual('ITransitionExpand')
         });
 
-        describe('methods', () => {
-            describe('attributesProvider.add()', () => {
-                it('should be defined', () => {
-                    expect(wrapper.vm.attributesProvider.add).toBeDefined();
-                });
-
-                it('should add rule to attributes provider', () => {
-                    wrapper.vm.attributesProvider.add(() => ({}));
-                    expect(wrapper.vm.attributesProvider.length).toEqual(1);
-                });
-
-                it('should add multiple rules to attributes provider', () => {
-                    wrapper.vm.attributesProvider.add(() => ({}));
-                    wrapper.vm.attributesProvider.add(() => ({}));
-                    expect(wrapper.vm.attributesProvider.length).toEqual(2);
-                });
-            });
+        it('should render correctly', () => {
+           expect(TransitionExpand.name).toEqual('ITransitionExpand')
         });
 
-        describe('computed', () => {
-            describe('attributes', () => {
-                it('should be defined', () => {
-                    expect(wrapper.vm.attributes).toBeDefined();
-                    expect(wrapper.vm.attributes).toEqual({});
-                });
-
-                it('should include inline attributes', () => {
-                    wrapper.setProps({ id: 'example' });
-
-                    expect(wrapper.vm.attributes).toHaveProperty('id');
-                    expect(wrapper.vm.attributes.id).toEqual('example');
-                });
-
-                it('should evaluate added attribute rule', () => {
-                    wrapper.vm.attributesProvider.add(() => ({ 'attr': 'value' }));
-
-                    expect(wrapper.vm.attributes).toHaveProperty('attr');
-                    expect(wrapper.vm.attributes.attr).toEqual('value');
-                });
-
-                it('should evaluate added attribute rules', () => {
-                    wrapper.vm.attributesProvider.add(() => ({ 'attr1': 'value1' }));
-                    wrapper.vm.attributesProvider.add(() => ({ 'attr2': 'value2' }));
-
-                    expect(wrapper.vm.attributes).toHaveProperty('attr1');
-                    expect(wrapper.vm.attributes.attr1).toEqual('value1');
-                    expect(wrapper.vm.attributes).toHaveProperty('attr2');
-                    expect(wrapper.vm.attributes.attr2).toEqual('value2');
-                });
-            });
+        it('should be functional', () => {
+           expect(wrapper.html()).toMatchSnapshot()
         });
+
+        // describe('data', () => {
+        //     describe('attributesProvider', () => {
+        //         it('should be defined', () => {
+        //             expect(wrapper.vm.attributesProvider).toBeDefined();
+        //             expect(wrapper.vm.attributesProvider.constructor).toEqual(Array);
+        //         });
+        //     });
+        // });
     });
 });
