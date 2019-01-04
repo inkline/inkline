c6e80502cec49e9ff3ffa2c7a8ad8bbd07aa2b3e
Alex <alex@grozav.com>
2019-01-04 15:15:10 +0200

    Fixed expand transition. Forced rerendering the component to fix it.

--- src/transitions/TransitionExpand/script.js
+++ src/transitions/TransitionExpand/script.js
@@ -9,18 +9,24 @@ export default {
             },
             on: {
                 enter(element) {
+                    const width = getComputedStyle(element).width;
+
+                    element.style.width = width;
                     element.style.position = 'absolute';
                     element.style.visibility = 'hidden';
                     element.style.height = 'auto';
 
                     const height = getComputedStyle(element).height;
+
+                    element.style.width = null;
                     element.style.position = null;
                     element.style.visibility = null;
-                    element.style.height = '0px';
+                    element.style.height = 0;
 
+                    getComputedStyle(element).height; // Force rerender element to set correct height
                     setTimeout(() => {
                         element.style.height = height;
-                    }, 0);
+                    });
                 },
                 afterEnter(element) {
                     element.style.height = 'auto';
@@ -28,9 +34,10 @@ export default {
                 leave(element) {
                     element.style.height = getComputedStyle(element).height;
 
+                    getComputedStyle(element).height; // Force rerender element to set correct height
                     setTimeout(() => {
-                        element.style.height = '0px';
-                    }, 0);
+                        element.style.height = 0;
+                    });
                 }
             }
         };
