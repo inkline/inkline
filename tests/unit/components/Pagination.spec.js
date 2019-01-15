ef57578d98a49816478c9304cb45ba5722547118
Alex Grozav <alex@grozav.com>
2019-01-15 18:12:07 +0200

    Added remaining pagination tests.

--- tests/unit/components/Pagination.spec.js
+++ tests/unit/components/Pagination.spec.js
@@ -118,6 +118,149 @@ describe('Components', () => {
                 it('should be defined', () => {
                     expect(wrapper.vm.pages).toBeDefined();
                 });
+
+                it('should add pages from 2 to pageCount if not quick previous and not quick next', () => {
+                    wrapper.setProps({ value: 1, items: 50, itemsPerPage: 10, limit: 5 });
+
+                    expect(wrapper.vm.pages).toEqual([2, 3, 4]);
+                });
+
+                it('should add pages from custom start to end if quick previous and not quick next', () => {
+                    wrapper.setProps({ value: 9, items: 100, itemsPerPage: 10, limit: 5 });
+
+                    expect(wrapper.vm.pages).toEqual([7, 8, 9]);
+                });
+
+                it('should add pages from start to custom end if not quick previous and quick next', () => {
+                    wrapper.setProps({ value: 3, items: 100, itemsPerPage: 10, limit: 5 });
+
+                    expect(wrapper.vm.pages).toEqual([2, 3, 4]);
+                });
+
+                it('should add pages from custom start to custom end if quick previous and quick next', () => {
+                    wrapper.setProps({ value: 6, items: 100, itemsPerPage: 10, limit: 5 });
+
+                    expect(wrapper.vm.pages).toEqual([5, 6, 7]);
+                });
+            });
+        });
+
+        describe('computed()', () => {
+            describe('next()', () => {
+                it('should be defined', () => {
+                    expect(wrapper.vm.next).toBeDefined();
+                });
+
+                it('should call onClick with next page index', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 1, items: 100, itemsPerPage: 10 });
+                    wrapper.vm.next();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(2);
+                });
+
+                it('should not call onClick if currentPage is page count', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 10, items: 100, itemsPerPage: 10 });
+                    wrapper.vm.next();
+
+                    expect(spy).not.toHaveBeenCalled();
+                });
+            });
+
+            describe('quickNext()', () => {
+                it('should be defined', () => {
+                    expect(wrapper.vm.quickNext).toBeDefined();
+                });
+
+                it('should call onClick jumping to currentPage + (limit - 2)', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 1, items: 100, itemsPerPage: 10, limit: 5 });
+                    wrapper.vm.quickNext();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(4);
+                });
+
+                it('should call onClick jumping to last page', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 8, items: 100, itemsPerPage: 10, limit: 5 });
+                    wrapper.vm.quickNext();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(10);
+                });
+            });
+
+            describe('previous()', () => {
+                it('should be defined', () => {
+                    expect(wrapper.vm.previous).toBeDefined();
+                });
+
+                it('should call onClick with previous page index', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 2, items: 100, itemsPerPage: 10 });
+                    wrapper.vm.previous();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(1);
+                });
+
+                it('should not call onClick if page index is 1', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 1, items: 100, itemsPerPage: 10 });
+                    wrapper.vm.previous();
+
+                    expect(spy).not.toHaveBeenCalled();
+                });
+            });
+
+            describe('quickPrevious()', () => {
+                it('should be defined', () => {
+                    expect(wrapper.vm.quickPrevious).toBeDefined();
+                });
+
+                it('should call onClick jumping to currentPage - (limit - 2)', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 5, items: 100, itemsPerPage: 10, limit: 5 });
+                    wrapper.vm.quickPrevious();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(2);
+                });
+
+                it('should call onClick jumping to last page', () => {
+                    const spy = jest.spyOn(wrapper.vm, 'onClick');
+
+                    wrapper.setProps({ value: 2, items: 100, itemsPerPage: 10, limit: 5 });
+                    wrapper.vm.quickPrevious();
+
+                    expect(spy).toHaveBeenCalled();
+                    expect(spy).toHaveBeenCalledWith(1);
+                });
+            });
+
+            describe('onClick()', () => {
+                it('should be defined', () => {
+                    expect(wrapper.vm.onClick).toBeDefined();
+                });
+
+                it('should emit input and change events', () => {
+                    const item = 1;
+
+                    wrapper.vm.onClick(item);
+
+                    expect(wrapper.emitted().input[0]).toEqual([item]);
+                    expect(wrapper.emitted().change[0]).toEqual([item]);
+                });
             });
         });
     });
