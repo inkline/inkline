742255c05d2339b432de99fad631dc969a9cdc7e
Alex Grozav <alex@grozav.com>
2019-02-13 19:36:33 +0200

    Added custom validator.

--- src/validators/custom.js
+++ src/validators/custom.js
@@ -1,18 +1,7 @@
-import isAlpha from 'validator/lib/isAlpha';
-
-export function alpha(value, options = {}) {
-    const process = (v) => {
-        v = String(v);
-
-        if (options.allowDashes) { v = v.replace(/-/g, ''); }
-        if (options.allowSpaces) { v = v.replace(/ /g, ''); }
-
-        return v;
-    };
-
+export function custom(value, options = { validator: () => true }) {
     if (value.constructor === Array) {
-        return value.every((v) => isAlpha(process(v), options.locale));
+        return value.every((v) => options.validator(v));
     }
 
-    return isAlpha(process(value), options.locale);
+    return options.validator(value);
 }
