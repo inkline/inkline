b34723b43f63f4b3d9ec12e92e8ab9d5fab5cca3
Alex Grozav <alex@grozav.com>
2019-02-13 19:39:54 +0200

    Renamed decimal validator to number.

--- tests/unit/validators/number.spec.js
+++ tests/unit/validators/number.spec.js
@@ -1,33 +1,33 @@
-import { decimal } from '@inkline/inkline/validators/decimal';
+import { number } from '@inkline/inkline/validators/number';
 
 describe('Validators', () => {
-    describe('decimal()', () => {
+    describe('number()', () => {
         it('should return true for positive int', () => {
-            expect(decimal('10')).toEqual(true);
+            expect(number('10')).toEqual(true);
         });
 
         it('should return true negative int', () => {
-            expect(decimal('-10')).toEqual(true);
+            expect(number('-10')).toEqual(true);
         });
 
         it('should return true for positive float', () => {
-            expect(decimal('10.99')).toEqual(true);
+            expect(number('10.99')).toEqual(true);
         });
 
         it('should return true negative float', () => {
-            expect(decimal('-10.99')).toEqual(true);
+            expect(number('-10.99')).toEqual(true);
         });
 
         it('should return false for characters other than 0-9', () => {
-            expect(decimal('-a10.99')).toEqual(false);
+            expect(number('-a10.99')).toEqual(false);
         });
 
         it('should return true if all array entries are decimal', () => {
-            expect(decimal(['10', '10.99', '-10', '-10.99'])).toEqual(true);
+            expect(number(['10', '10.99', '-10', '-10.99'])).toEqual(true);
         });
 
         it('should return false if not all array entries are decimal', () => {
-            expect(decimal(['10', '10.99', 'string', '-10', '-10.99'])).toEqual(false);
+            expect(number(['10', '10.99', 'string', '-10', '-10.99'])).toEqual(false);
         });
     });
 });
