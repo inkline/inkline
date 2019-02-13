42b7072701c18201a9decb2c7589e506cdb665e1
Alex Grozav <alex@grozav.com>
2019-02-13 19:39:26 +0200

    Added tests for custom validator.

---
+++ tests/unit/validators/custom.spec.js
@@ -0,0 +1,17 @@
+import { custom } from '@inkline/inkline/validators/custom';
+
+describe('Validators', () => {
+    describe('custom()', () => {
+        const validator = (x) => x > 0;
+
+        it('should apply validator function', () => {
+            expect(custom(10, { validator })).toEqual(true);
+            expect(custom(-10, { validator })).toEqual(false);
+        });
+
+        it('should apply validator function to every value of array', () => {
+            expect(custom([1, 2, 3], { validator })).toEqual(true);
+            expect(custom([0, 1, 2], { validator })).toEqual(false);
+        });
+    });
+});
