79fe586ddccf18f3ab2314747c8780bc83d43e90
Alex Grozav <alex@grozav.com>
2019-02-14 14:10:05 +0200

    Fixed nuxt build and updated alias resolution.

--- packages/inkline.io/nuxt.config.js
+++ packages/inkline.io/nuxt.config.js
@@ -62,43 +62,48 @@ module.exports = {
         ** You can extend webpack config here
         */
         extend(config, ctx) {
-            // Run ESLint on save
             if (ctx.isDev && ctx.isClient) {
+                // Run ESLint on save
                 config.module.rules.push({
                     enforce: 'pre',
                     test: /\.(js|vue)$/,
                     loader: 'eslint-loader',
                     exclude: /node_modules\/(?!@inkline)/
                 });
-
-                config.module.rules.push({
-                    test: /\.md$/i,
-                    loader: 'markdown-complete-loader',
-                    options: {
-                        configFile: path.join(__dirname, 'markdown.config.js')
-                    }
-                });
-
-                [].concat(...config.module.rules
-                    .find((e) => e.test.toString().match(/\.styl/))
-                    .oneOf
-                    .map((e) => e.use.filter(e => e.loader === 'stylus-loader'))
-                ).forEach((stylus) => {
-                    Object.assign(stylus.options, {
-                        import: [
-                            path.join(__dirname, 'css/config/index.styl')
-                        ]
-                    })
-                });
-
-                config.resolve.symlinks = true;
-
-                config.resolve.alias['@components'] = path.join(__dirname, 'components');
-                config.resolve.alias['@directives'] = path.join(__dirname, 'directives');
-                config.resolve.alias['@resources'] = path.join(__dirname, 'resources');
-                config.resolve.alias['@routes'] = path.join(__dirname, 'routes');
-                config.resolve.alias['@views'] = path.join(__dirname, 'views');
             }
+
+
+
+            // Add markdown templates support
+            config.module.rules.push({
+                test: /\.md$/i,
+                loader: 'markdown-complete-loader',
+                options: {
+                    configFile: path.join(__dirname, 'markdown.config.js')
+                }
+            });
+
+            // Add stylus config override
+            [].concat(...config.module.rules
+                .find((e) => e.test.toString().match(/\.styl/))
+                .oneOf
+                .map((e) => e.use.filter(e => e.loader === 'stylus-loader'))
+            ).forEach((stylus) => {
+                Object.assign(stylus.options, {
+                    import: [
+                        path.join(__dirname, 'css/config/index.styl')
+                    ]
+                })
+            });
+
+            config.resolve.symlinks = true;
+
+            config.resolve.alias['@components'] = path.join(__dirname, 'components');
+            config.resolve.alias['@directives'] = path.join(__dirname, 'directives');
+            config.resolve.alias['@resources'] = path.join(__dirname, 'resources');
+            config.resolve.alias['@routes'] = path.join(__dirname, 'routes');
+            config.resolve.alias['@views'] = path.join(__dirname, 'views');
+            config.resolve.alias['@inkline/inkline'] = '@inkline/inkline/src';
         },
         babel: {
             babelrc: true
