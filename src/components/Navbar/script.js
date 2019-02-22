abe9026337132ba3144647a96eab75de65c20eea
Alex Grozav <alex@grozav.com>
2019-02-22 19:22:30 +0200

    Updated navbar to collapse responsively.

--- src/components/Navbar/script.js
+++ src/components/Navbar/script.js
@@ -8,6 +8,8 @@ import ClassesProviderMixin from '@inkline/inkline/mixins/components/providers/C
 import SizePropertyMixin from '@inkline/inkline/mixins/components/properties/SizePropertyMixin';
 import VariantPropertyMixin from '@inkline/inkline/mixins/components/properties/VariantPropertyMixin';
 
+import { breakpoints } from '@inkline/inkline/constants';
+
 export default {
     name: 'INavbar',
     mixins: [
@@ -22,10 +24,70 @@ export default {
         IRow,
         IColumn
     },
+    provide() {
+        const navbar = {};
+
+        Object.defineProperty(navbar, 'collapsed', {
+            enumerable: true,
+            get: () => this.collapsed,
+        });
+
+        Object.defineProperty(navbar, 'collapse', {
+            enumerable: true,
+            get: () => this.collapse,
+        });
+
+        return { navbar };
+    },
+    data() {
+        return {
+            collapsed: false,
+            windowWidth: window.innerWidth
+        };
+    },
     props: {
         fluid: {
             type: Boolean,
             default: false
+        },
+        collapse: {
+            type: String,
+            default: 'md'
+        },
+        toggleAnimation: {
+            type: String,
+            default: 'close'
         }
-    }
+    },
+    methods: {
+        toggleCollapse() {
+            this.collapsed = !this.collapsed;
+        },
+        onWindowResize() {
+            const windowWidth = window.innerWidth;
+
+            if (this.windowWidth <= breakpoints[this.collapse][1] && windowWidth > breakpoints[this.collapse][1]) {
+                this.collapsed = false;
+            }
+
+            this.windowWidth = windowWidth;
+        }
+    },
+    created () {
+        this.classesProvider.add(() => ({
+            '-collapsed': this.collapsed,
+            [`-collapse-${this.collapse}`]: Boolean(this.collapse)
+        }));
+
+        this.classesProvider.add('hamburger', () => ({
+            '-active': this.collapsed,
+            [`-${this.toggleAnimation}`]: true
+        }));
+
+        window.addEventListener('resize', this.onWindowResize);
+        this.onWindowResize();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.onWindowResize)
+    },
 };
--- src/components/NavbarItems/script.js
+++ src/components/NavbarItems/script.js
@@ -1,3 +1,27 @@
+import ITransitionExpand from '@inkline/inkline/transitions/TransitionExpand';
+import { breakpoints } from "@inkline/inkline/constants";
+
 export default {
-    name: 'INavbarItems'
+    name: 'INavbarItems',
+    inject: [ 'navbar' ],
+    components: {
+        ITransitionExpand
+    },
+    data() {
+        return {
+            collapsible: false
+        }
+    },
+    methods: {
+        onWindowResize() {
+            this.collapsible = window.innerWidth <= breakpoints[this.navbar.collapse][1];
+        }
+    },
+    created () {
+        window.addEventListener('resize', this.onWindowResize);
+        this.onWindowResize();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.onWindowResize)
+    }
 };
