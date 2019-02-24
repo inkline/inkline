89b7e85c2711ce54ff651e3b5352f1bb217d8e4f
Alex Grozav <alex@grozav.com>
2019-02-24 19:57:44 +0200

    Updated list group item to use Linkable.

--- src/components/ListGroupItem/script.js
+++ src/components/ListGroupItem/script.js
@@ -1,27 +1,23 @@
+import Linkable from '../Linkable';
+
 import AttributesProviderMixin from '@inkline/inkline/mixins/components/providers/AttributesProviderMixin';
 import ClassesProviderMixin from '@inkline/inkline/mixins/components/providers/ClassesProviderMixin';
 
-import ActivePropertyMixin from '@inkline/inkline/mixins/components/properties/ActivePropertyMixin';
 import DisabledPropertyMixin from '@inkline/inkline/mixins/components/properties/DisabledPropertyMixin';
 
 export default {
     name: 'IListGroupItem',
+    extends: Linkable,
     mixins: [
         AttributesProviderMixin,
         ClassesProviderMixin,
 
-        ActivePropertyMixin,
         DisabledPropertyMixin
     ],
     props: {
         tag: {
             type: String,
             default: 'div'
-        }
-    },
-    computed: {
-        isTag () {
-            return this.attributes.to ? 'router-link' : this.attributes.href ? 'a' : this.tag;
         }
     }
 };
--- tests/unit/components/ListGroupItem.spec.js
+++ tests/unit/components/ListGroupItem.spec.js
@@ -26,40 +26,5 @@ describe('Components', () => {
                 });
             });
         });
-
-        describe('computed', () => {
-            describe('isTag()', () => {
-                it('should be defined', () => {
-                    expect(wrapper.vm.isTag).toBeDefined();
-                });
-
-                it('should be <div> tag by default', () => {
-                    expect(wrapper.vm.isTag).toEqual('div');
-                });
-
-                it('should be <a> tag if "href" attribute is provided', () => {
-                    wrapper = shallowMount(ListGroupItem, {
-                        propsData: {
-                            href: true
-                        }
-                    });
-
-                    expect(wrapper.vm.isTag).toEqual('a');
-                });
-
-                it('should be <router-link> tag if "to" attribute is provided', () => {
-                    wrapper = shallowMount(ListGroupItem, {
-                        components: {
-                            RouterLink
-                        },
-                        propsData: {
-                            to: true
-                        }
-                    });
-
-                    expect(wrapper.vm.isTag).toEqual('router-link');
-                });
-            });
-        });
     });
 });
