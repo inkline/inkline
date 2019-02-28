85a151af5dd1d6ddaa6268cc2b319959581b63a0
Alex Grozav <alex@grozav.com>
2019-02-28 16:23:13 +0200

    Updated package version.

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -4,19 +4,13 @@ rm dist/Inkline.common.js
 rm dist/Inkline.common.js.map
 rm dist/Inkline.umd.min.js
 rm dist/Inkline.umd.min.js.map
-# rm dist/Inkline.umd.js
-# rm dist/Inkline.umd.js.map
 
 mv dist/Inkline.umd.js dist/inkline.js
 mv dist/Inkline.umd.js.map dist/inkline.js.map
 sed -i '' 's/Inkline.umd.js.map/inkline.js.map/g' 'dist/inkline.js'
 
-# mv dist/Inkline.umd.min.js dist/inkline.js
-# mv dist/Inkline.umd.min.js.map dist/inkline.js.map
-# sed -i '' 's/Inkline.umd.min.js.map/inkline.js.map/g' 'dist/inkline.js'
-
 mv dist/Inkline.css dist/inkline.css
 
-# git add dist/
-# git commit -m "Generated dist folder."
+git add dist/
+git commit -m "Generated dist folder."
 exit 0
