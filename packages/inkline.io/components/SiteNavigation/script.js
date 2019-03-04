35f54ce1e36152984a48d91f7234e70d8ed0d794
Alex Grozav <alex@grozav.com>
2019-03-04 23:48:47 +0200

    Added algolia search input.

--- packages/inkline.io/components/SiteNavigation/script.js
+++ packages/inkline.io/components/SiteNavigation/script.js
@@ -1,3 +1,14 @@
+import algoliasearch from "algoliasearch";
+import 'instantsearch.css/themes/algolia.css';
+
 export default {
-    name: 'SiteNavigation'
+    name: 'SiteNavigation',
+    data () {
+        return {
+            popperOptions: {
+                positionFixed: true
+            },
+            searchClient: algoliasearch(process.env.ALGOLIA_APPLICATION_ID, process.env.ALGOLIA_API_KEY)
+        };
+    }
 };
