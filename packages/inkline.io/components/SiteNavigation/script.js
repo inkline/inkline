203b9d22329a50e5df300051be73a727eefbc4c3
Alex Grozav <alex@grozav.com>
2019-03-07 16:50:27 +0200

    Updated algolia script to separate doc sections into separate indices.

--- packages/inkline.io/components/SiteNavigation/script.js
+++ packages/inkline.io/components/SiteNavigation/script.js
@@ -8,7 +8,19 @@ export default {
             popperOptions: {
                 positionFixed: true
             },
+            searchIndices: [
+                { label: 'Introduction', value: 'introduction' },
+                { label: 'Core', value: 'core' },
+                { label: 'Forms', value: 'forms' },
+                { label: 'Components', value: 'components' },
+            ],
+            searchKeymap: { show: ['enter'], hide: ['enter'] },
             searchClient: algoliasearch(process.env.ALGOLIA_APPLICATION_ID, process.env.ALGOLIA_API_KEY)
         };
+    },
+    methods: {
+        updateDropdown() {
+            this.$refs.dropdown.$emit('change');
+        }
     }
 };
