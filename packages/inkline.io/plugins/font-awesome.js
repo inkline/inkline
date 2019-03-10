de892d65468a2a8b60a1e97381f8060471483ed3
Alex Grozav <alex@grozav.com>
2019-03-10 22:58:31 +0200

    Updated search box design. Working on responsive navigation.

--- packages/inkline.io/plugins/font-awesome.js
+++ packages/inkline.io/plugins/font-awesome.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
-import { faInfoCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faInfoCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+fontAwesomeLibrary.add(faBars);
 fontAwesomeLibrary.add(faInfoCircle);
 fontAwesomeLibrary.add(faSearch);
 fontAwesomeLibrary.add(faGithub);
