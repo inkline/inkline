c868b271233ad653338d0a8e2fddbcf65d023f04
Alex Grozav <alex@grozav.com>
2019-03-10 19:14:39 +0200

    Added tests for query helpers.

--- src/helpers/query.js
+++ src/helpers/query.js
@@ -1,3 +1,40 @@
+
+
+/**
+ * Find component by component instance name.
+ * Mainly used for searching first matching child in component slots.
+ *
+ * @param items
+ * @param name
+ * @param maxDepth
+ * @param currentDepth
+ * @returns {Array}
+ */
+export function querySelector(items, name, maxDepth = 3, currentDepth = 0) {
+    let result;
+
+    for (const item of items) {
+        const children = item.$children || (item.componentInstance || {}).$children || item.children;
+
+        if ((item.$options || {}).name === name ||
+            ((item.$options || {}).extends || {}).name === name ||
+            ((item.componentInstance || {}).$options || {}).name === name ||
+            (((item.componentInstance || {}).$options || {}).extends || {}).name === name) {
+            result = item;
+        }
+
+        if (children && children.length > 0 && currentDepth < maxDepth && !result) {
+            result = querySelector(children, name, maxDepth, currentDepth + 1);
+        }
+
+        if (result) {
+            break;
+        }
+    }
+
+    return result;
+}
+
 /**
  * Find components by component instance name.
  * Mainly used for filtering and searching children in component slots.
@@ -27,39 +64,4 @@ export function querySelectorAll(items, name, maxDepth = 3, currentDepth = 0) {
     }
 
     return results;
-}
-
-/**
- * Find component by component instance name.
- * Mainly used for searching first matching child in component slots.
- *
- * @param items
- * @param name
- * @param maxDepth
- * @param currentDepth
- * @returns {Array}
- */
-export function querySelector(items, name, maxDepth = 3, currentDepth = 0) {
-    let result;
-
-    for (const item of items) {
-        const children = item.$children || (item.componentInstance || {}).$children || item.children;
-
-        if ((item.$options || {}).name === name ||
-            ((item.$options || {}).extends || {}).name === name ||
-            ((item.componentInstance || {}).$options || {}).name === name ||
-            (((item.componentInstance || {}).$options || {}).extends || {}).name === name) {
-            result = item;
-        }
-
-        if (result) {
-            break;
-        }
-
-        if (children && children.length > 0 && currentDepth < maxDepth) {
-            result = querySelectorAll(children, name, maxDepth, currentDepth + 1);
-        }
-    }
-
-    return result;
 }
---
+++ tests/unit/helpers/query.spec.js
@@ -0,0 +1,111 @@
+import { querySelector, querySelectorAll } from '@inkline/inkline/src/helpers/query';
+
+describe('Helpers', () => {
+    describe('querySelector()', () => {
+        const items = [
+            {},
+            { $options: { name: '1' } },
+            { $options: { extends: { name: '2' } } },
+            { componentInstance: { $options: { name: '3' } } },
+            { componentInstance: { $options: { extends: { name: '4' } } } },
+            { children: [{ $options: { name: '5' } }] },
+            { $children: [{ $options: { name: '6' } }] },
+            { componentInstance: { $children: [{ $options: { name: '7' } }] } },
+            { $children: [{ $children: [{ $children: [{ $options: { name: '8' } }] }] }] },
+        ];
+
+        it('should return undefined if no items in array', () => {
+            expect(querySelector([])).toEqual(undefined)
+        });
+
+        it('should return item if $options.name matches given name', () => {
+            expect(querySelector(items, '1')).toEqual(items[1]);
+        });
+
+        it('should return item if $options.$extends.name matches given name', () => {
+            expect(querySelector(items, '2')).toEqual(items[2]);
+        });
+
+        it('should return item if componentInstance.$options.name matches given name', () => {
+            expect(querySelector(items, '3')).toEqual(items[3]);
+        });
+
+        it('should return item if componentInstance.$options.$extends.name matches given name', () => {
+            expect(querySelector(items, '4')).toEqual(items[4]);
+        });
+
+        it('should return item if children[0] matches given name', () => {
+            expect(querySelector(items, '5')).toEqual(items[5].children[0]);
+        });
+
+        it('should return item if $children[0] matches given name', () => {
+            expect(querySelector(items, '6')).toEqual(items[6].$children[0]);
+        });
+
+        it('should return item if componentInstance.$children[0] matches given name', () => {
+            expect(querySelector(items, '7')).toEqual(items[7].componentInstance.$children[0]);
+        });
+
+        it('should return undefined if item below given search depth', () => {
+            expect(querySelector(items, '8', 2)).toEqual(undefined);
+        });
+
+        it('should return item if item above given search depth', () => {
+            expect(querySelector(items, '8', 3)).toEqual({ $options: { name: '8' } });
+        });
+    });
+
+    describe('querySelectorAll()', () => {
+        const items = [
+            {},
+            { $options: { name: '1' } },
+            { $options: { extends: { name: '2' } } },
+            { componentInstance: { $options: { name: '3' } } },
+            { componentInstance: { $options: { extends: { name: '4' } } } },
+            { children: [{ $options: { name: '5' } }] },
+            { $children: [{ $options: { name: '6' } }] },
+            { componentInstance: { $children: [{ $options: { name: '7' } }] } },
+            { $children: [{ $children: [{ $children: [{ $options: { name: '8' } }] }] }] },
+        ];
+
+        it('should return undefined if no items in array', () => {
+            expect(querySelectorAll([])).toEqual([])
+        });
+
+        it('should return item if $options.name matches given name', () => {
+            expect(querySelectorAll(items, '1')).toEqual([items[1]]);
+        });
+
+        it('should return item if $options.$extends.name matches given name', () => {
+            expect(querySelectorAll(items, '2')).toEqual([items[2]]);
+        });
+
+        it('should return item if componentInstance.$options.name matches given name', () => {
+            expect(querySelectorAll(items, '3')).toEqual([items[3]]);
+        });
+
+        it('should return item if componentInstance.$options.$extends.name matches given name', () => {
+            expect(querySelectorAll(items, '4')).toEqual([items[4]]);
+        });
+
+        it('should return item if children[0] matches given name', () => {
+            expect(querySelectorAll(items, '5')).toEqual([items[5].children[0]]);
+        });
+
+        it('should return item if $children[0] matches given name', () => {
+            expect(querySelectorAll(items, '6')).toEqual([items[6].$children[0]]);
+        });
+
+        it('should return item if componentInstance.$children[0] matches given name', () => {
+            expect(querySelectorAll(items, '7')).toEqual([items[7].componentInstance.$children[0]]);
+        });
+
+        it('should return undefined if item below given search depth', () => {
+            expect(querySelectorAll(items, '8', 2)).toEqual([]);
+        });
+
+        it('should return item if item above given search depth', () => {
+            expect(querySelectorAll(items, '8', 3)).toEqual([{ $options: { name: '8' } }]);
+        });
+    });
+});
