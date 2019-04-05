7bca472ef6fb8b82754b70e753454a63da0edadf
Alex <alex@grozav.com>
2019-04-05 16:25:30 +0300

    Added back normal import for Vue InstantSearch.

--- packages/inkline.io/components/SiteSearch/script.js
+++ packages/inkline.io/components/SiteSearch/script.js
@@ -5,6 +5,8 @@ import 'instantsearch.css/themes/algolia.css';
 const ALGOLIA_APPLICATION_ID = '20XO7MC0CB';
 const ALGOLIA_API_KEY = 'd220f0c7ea35fe42306fe72fb00f3a91';
 
+const searchClient = algoliasearch(ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY);
+
 export default {
     name: 'SiteSearch',
     data () {
@@ -16,7 +18,7 @@ export default {
                 { label: 'Components', value: 'components' },
             ],
             searchKeymap: { show: ['enter'], hide: ['enter'] },
-            searchClient: algoliasearch(ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY)
+            searchClient
         };
     },
     methods: {
