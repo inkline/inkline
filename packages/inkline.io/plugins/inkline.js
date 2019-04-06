807926e0000dbf4e99a4eaee3a53fdfabacac615
Alex Grozav <alex@grozav.com>
2019-04-06 18:50:42 +0300

    Updated form validation docs.

--- packages/inkline.io/plugins/inkline.js
+++ packages/inkline.io/plugins/inkline.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Inkline from '@inkline/inkline'
+import InklineValidation from '@inkline/validation'
 import '@inkline/inkline/index.styl';
 
 Vue.use(Inkline);
+Vue.use(InklineValidation);
