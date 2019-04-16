c00019eb63e293253227fb6f9fb2f9bb8189534b
Alex Grozav <alex@grozav.com>
2019-04-16 16:46:19 +0300

    Updated css inclusion, vue config and package versions.

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,16 +1,16 @@
-rm dist/demo.html
+rm ./dist/demo.html
 
-rm dist/Inkline.common.js
-rm dist/Inkline.common.js.map
-rm dist/Inkline.umd.min.js
-rm dist/Inkline.umd.min.js.map
+rm ./dist/Inkline.common.js
+rm ./dist/Inkline.common.js.map
+rm ./dist/Inkline.umd.min.js
+rm ./dist/Inkline.umd.min.js.map
 
-mv dist/Inkline.umd.js dist/inkline.js
-mv dist/Inkline.umd.js.map dist/inkline.js.map
-sed -i '' 's/Inkline.umd.js.map/inkline.js.map/g' 'dist/inkline.js'
+mv ./dist/Inkline.umd.js dist/inkline.js
+mv ./dist/Inkline.umd.js.map dist/inkline.js.map
+sed -i '' 's/Inkline.umd.js.map/inkline.js.map/g' './dist/inkline.js'
 
-mv dist/Inkline.css dist/inkline.css
+mv ./dist/Inkline.css ./dist/inkline.css
 
-git add dist/
+git add ./dist/
 git commit -m "Generated dist folder."
 exit 0
