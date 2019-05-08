35726a0670f44a4bbae0c997712ec0d587a299f5
Alex Grozav <alex@grozav.com>
2019-05-08 23:03:39 +0300

    Updated inkline version, sitemap and search indices.

--- packages/inkline.io/plugins/inkline.js
+++ packages/inkline.io/plugins/inkline.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import Inkline from '@inkline/inkline'
-import InklineValidation from '@inkline/validation'
+import Inkline from '@inkline/inkline/src/index'
+import InklineValidation from '@inkline/inkline/src/validation'
 import '@inkline/inkline/index.styl';
 
 Vue.use(Inkline);
