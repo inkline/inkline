0c647dcfff88d545ac68e92f3be2fc651ef6b954
Alex Grozav <alex@grozav.com>
2019-06-01 11:32:23 +0200

    Updated generator to use api.entryFile. Updated package description.

--- packages/vue-cli-plugin-inkline/generator/index.js
+++ packages/vue-cli-plugin-inkline/generator/index.js
@@ -1,27 +1,30 @@
 module.exports = (api, options, rootOptions) => {
-  api.extendPackage({
-    dependencies: {
-      '@inkline/inkline': '^1.14'
+	api.extendPackage({
+		dependencies: {
+			'@inkline/inkline': '^1.14'
 		}
 	});
 
+	// Add imports
+	api.injectImports(api.entryFile, options.stylus ?
+		`import '@inkline/inkline/dist/inkline.css'` :
+		`import '@inkline/inkline/src/index.styl'`);
+	api.injectImports(api.entryFile, `import Inkline from '@inkline/inkline'`);
+
 	api.onCreateComplete(() => {
-		const fs       = require('fs');
-		const ext      = api.hasPlugin('typescript') ? 'ts' : 'js';
-		const mainPath = api.resolve(`./src/main.${ext}`);
+		const fs   = require('fs');
+		const isTs = api.hasPlugin('typescript') ? 'ts' : 'js';
 
 		// Read and get content
-		let content = fs.readFileSync(mainPath, { encoding: 'utf-8' });
+		let content = fs.readFileSync(api.entryFile, { encoding: 'utf-8' });
 		const lines = content.split(/\r?\n/g).reverse();
 
 		// Inject imports
 		const lastImportIndex = lines.findIndex(line => line.match(/^import/));
-		lines[lastImportIndex] += `\nimport '@inkline/inkline/dist/inkline.css';`
-		lines[lastImportIndex] += `\nimport Inkline from '@inkline/inkline';`;
 		lines[lastImportIndex] += `\n\nVue.use(Inkline);`;
 
 		// Write back
 		content = lines.reverse().join(`\n`);
-		fs.writeFileSync(mainPath, content, { encoding: 'utf-8' });
+		fs.writeFileSync(api.entryFile, content, { encoding: 'utf-8' });
 	})
-}
+};
--- packages/vue-cli-plugin-inkline/index.js
+++ packages/vue-cli-plugin-inkline/index.js
@@ -1,2 +1,1 @@
-module.exports = (api, opts) => {}
-
+module.exports = (api, opts) => {};
