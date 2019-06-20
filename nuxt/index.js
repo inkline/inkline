8442b42bc997c5e38daa343e16777094e3cd85aa
Alex Grozav <alex@grozav.com>
2019-06-20 08:57:20 +0300

    Updated nuxt plugin configuration.

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,39 +1,44 @@
 const { resolve } = require('path');
 
+const defaultOptions = {
+    stylus: false,
+    treeShaking: false
+};
 
-const srcIndex = '@inkline/inkline/src/index.js';
-
-
-module.exports = function nuxtBootstrapVue(moduleOptions = {}) {
+module.exports = function InklineNuxt(moduleOptions = {}) {
     this.nuxt.hook('build:before', () => {
         const options = {
-            stylus: false,
-            treeShaking: false,
+            ...defaultOptions,
             ...this.options.inkline,
             ...moduleOptions
         };
 
-        // Ensure we have arrays
         this.options.css = [].concat(this.options.css || []);
-        this.options.build.transpile = [].concat(this.options.build.transpile || []);
-
         if (options.stylus) {
-            this.options.css.unshift('@inkline/inkline/src/index.styl')
+            this.options.css.unshift('@inkline/inkline/src/index.styl');
         } else {
-            this.options.css.unshift('@inkline/inkline/dist/inkline.css')
+            this.options.css.unshift('@inkline/inkline/dist/inkline.css');
         }
 
+        this.options.build.transpile = [].concat(this.options.build.transpile || []);
         if (options.stylus || options.treeShaking) {
-            this.options.build.transpile.push('@inkline/inkline/src')
+            this.options.build.transpile.push('@inkline/inkline');
         }
+
+        const inklineOptions = { ...options };
+        delete inklineOptions.stylus;
+        delete inklineOptions.treeShaking;
 
         // Register plugin, passing options to plugin template
         this.addPlugin({
             src: resolve(__dirname, 'plugin.template.js'),
             fileName: 'inkline.js',
-            options
-        })
-    })
-}
+            options: {
+                inkline: inklineOptions,
+                treeShaking: options.treeShaking
+            }
+        });
+    });
+};
 
-module.exports.meta = require('../package.json')
+module.exports.meta = require('../package.json');
