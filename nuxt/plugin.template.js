09eb904edcb9c6177542ee6a3f2561abfb097e32
Alex Grozav <alex@grozav.com>
2019-06-21 17:15:49 +0300

    Added form validation code to template. (WIP)

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,24 +1,34 @@
 import Vue from 'vue';
 
+
 <% if (!options.treeShaking) { %>
 import Inkline from '@inkline/inkline';
 
 Vue.use(Inkline, <%= JSON.stringify(options.inkline.components ? options.inkline : {}, undefined, 2) %>);
-<% } %>
-
-<% if (options.treeShaking) { %>
+<% } else { %>
 <% if (options.inkline.components) { %>
 import {
-    <%= ['$form'].concat(options.inkline.components).join(',\n  ') %>
+    <%= ['$form'].concat(options.inkline.components).join(',\n    ') %>
 } from '@inkline/inkline/src/index';
+<% if (options.validation) { %>
+import InklineValidation from '@inkline/inkline/dist/validation';
+import InklineValidation from '@inkline/inkline/src/validation';
+<% } %>
 
-Vue.directive('$form', $form);
+Vue.prototype.$form = $form;
 
 <%= options.components.map((component) => `Vue.component('${component}', ${component})`).join('\n') %>
-<% } %>
-<% if (!options.inkline.components) { %>
+<% } else { %>
 import Inkline from '@inkline/inkline/src/index';
+<% if (options.validation) { %>
+import InklineValidation from '@inkline/inkline/dist/validation';
+import InklineValidation from '@inkline/inkline/src/validation';
+<% } %>
 
 Vue.use(Inkline, <%= JSON.stringify(options.inkline, undefined, 2) %>);
 <% } %>
+<% } %>
+
+<% if (options.validation) { %>
+Vue.use(InklineValidation);
 <% } %>
