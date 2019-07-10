bd38922a24365c8d403b2810a2a0c6f1145357b6
Manuel Tancoigne <m.tancoigne@gmail.com>
2019-07-10 23:01:19 +0200

    SCSS: Remplaced stylus imports in remaining files.

--- packages/inkline.io/plugins/inkline.js
+++ packages/inkline.io/plugins/inkline.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Inkline from '@inkline/inkline/src/index'
 import InklineValidation from '@inkline/inkline/src/validation'
-import '@inkline/inkline/index.styl';
+import '@inkline/inkline/index.scss';
 
 Vue.use(Inkline);
 Vue.use(InklineValidation);
