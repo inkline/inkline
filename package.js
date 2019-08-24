8dd44f12397646d6196b4dc8ffa142d91544b786
Alex Grozav <alex@grozav.com>
2019-08-24 11:59:11 +0200

    Updated version.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
     name: "inkline:inkline",
-    version: "1.16.1",
+    version: "1.16.2",
     summary:
         "Inkline is a modern UI/UX framework for Vue.js, designed for creating flawless content-rich responsive web applications",
     git: "https://github.com/inkline/inkline.git",
