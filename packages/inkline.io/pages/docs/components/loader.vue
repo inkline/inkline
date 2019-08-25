c82332a29c68d3438856fb70f8c5a9e1222ecba5
Alex Grozav <alex@grozav.com>
2019-08-25 16:00:21 +0200

    Added loader component docs.

---
+++ packages/inkline.io/pages/docs/components/loader.vue
@@ -0,0 +1,16 @@
+<template src="./loader.md" lang="md"></template>
+<style src="./loader.scss" lang="scss"></style>
+<script>
+import View from '@components/View';
+import { head } from "@helpers/head";
+
+export default {
+    name: 'LoaderView',
+    layout: 'documentation',
+    extends: View,
+    head: head({
+        title: 'Loader',
+        description: 'Provide a loading state for a component or page using Inkline\'s customizable loading spinner.'
+    })
+};
+</script>
