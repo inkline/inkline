e138a97af24c2554aa597766630119f1f65725a8
Alex Grozav <alex@grozav.com>
2019-09-25 19:38:55 +0300

    Updated navbar to emit, listen and react to sidebar toggle events.

--- packages/inkline.io/components/Navbar/script.js
+++ packages/inkline.io/components/Navbar/script.js
@@ -4,5 +4,20 @@ export default {
     name: 'Navbar',
     components: {
         SiteSearch
+    },
+    data() {
+        return {
+            sidebarOpen: false
+        };
+    },
+    methods: {
+        toggleSidebar() {
+            this.$emit('toggle');
+        }
+    },
+    mounted() {
+        this.$on('sidebarToggle', (value) => {
+            this.sidebarOpen = value;
+        })
     }
 };
