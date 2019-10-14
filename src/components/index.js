8fdb46a2f776e406926ad3acf6f56c9cfea7ce4c
Alex Grozav <alex@grozav.com>
2019-10-14 15:43:23 +0300

    Changed exports to support better tree shaking.

--- src/components/index.js
+++ src/components/index.js
@@ -1,117 +1,57 @@
-import IAlert from '@inkline/inkline/src/components/Alert';
-import IBadge from '@inkline/inkline/src/components/Badge';
-import IBreadcrumb from '@inkline/inkline/src/components/Breadcrumb';
-import IBreadcrumbItem from '@inkline/inkline/src/components/BreadcrumbItem';
-import IButton from '@inkline/inkline/src/components/Button';
-import IButtonGroup from '@inkline/inkline/src/components/ButtonGroup';
-import ICard from '@inkline/inkline/src/components/Card';
-import ICollapsible from '@inkline/inkline/src/components/Collapsible';
-import ICollapsibleItem from '@inkline/inkline/src/components/CollapsibleItem';
-import IColumn from '@inkline/inkline/src/components/Column';
-import IContainer from '@inkline/inkline/src/components/Container';
-import IDropdown from '@inkline/inkline/src/components/Dropdown';
-import IDropdownDivider from '@inkline/inkline/src/components/DropdownDivider';
-import IDropdownItem from '@inkline/inkline/src/components/DropdownItem';
-import IDropdownMenu from '@inkline/inkline/src/components/DropdownMenu';
-import IHamburgerMenu from '@inkline/inkline/src/components/HamburgerMenu';
-import IHeader from '@inkline/inkline/src/components/Header';
-import IIcon from '@inkline/inkline/src/components/Icon';
-import ILayout from '@inkline/inkline/src/components/Layout';
-import ILayoutAside from '@inkline/inkline/src/components/LayoutAside';
-import ILayoutContent from '@inkline/inkline/src/components/LayoutContent';
-import ILayoutFooter from '@inkline/inkline/src/components/LayoutFooter';
-import ILayoutHeader from '@inkline/inkline/src/components/LayoutHeader';
-import IListGroup from '@inkline/inkline/src/components/ListGroup';
-import IListGroupItem from '@inkline/inkline/src/components/ListGroupItem';
-import ILoader from '@inkline/inkline/src/components/Loader';
-import IMedia from '@inkline/inkline/src/components/Media';
-import IModal from '@inkline/inkline/src/components/Modal';
-import INav from '@inkline/inkline/src/components/Nav';
-import INavItem from '@inkline/inkline/src/components/NavItem';
-import INavbar from '@inkline/inkline/src/components/Navbar';
-import INavbarBrand from '@inkline/inkline/src/components/NavbarBrand';
-import INavbarItems from '@inkline/inkline/src/components/NavbarItems';
-import IRow from '@inkline/inkline/src/components/Row';
-import IPagination from '@inkline/inkline/src/components/Pagination';
-import IPopover from '@inkline/inkline/src/components/Popover';
-import IProgress from '@inkline/inkline/src/components/Progress';
-import IProgressBar from '@inkline/inkline/src/components/ProgressBar';
-import ITable from '@inkline/inkline/src/components/Table';
-import ITooltip from '@inkline/inkline/src/components/Tooltip';
+export { default as IAlert } from '@inkline/inkline/src/components/Alert';
+export { default as IBadge } from '@inkline/inkline/src/components/Badge';
+export { default as IBreadcrumb } from '@inkline/inkline/src/components/Breadcrumb';
+export { default as IBreadcrumbItem } from '@inkline/inkline/src/components/BreadcrumbItem';
+export { default as IButton } from '@inkline/inkline/src/components/Button';
+export { default as IButtonGroup } from '@inkline/inkline/src/components/ButtonGroup';
+export { default as ICard } from '@inkline/inkline/src/components/Card';
+export { default as ICollapsible } from '@inkline/inkline/src/components/Collapsible';
+export { default as ICollapsibleItem } from '@inkline/inkline/src/components/CollapsibleItem';
+export { default as IColumn } from '@inkline/inkline/src/components/Column';
+export { default as IContainer } from '@inkline/inkline/src/components/Container';
+export { default as IDropdown } from '@inkline/inkline/src/components/Dropdown';
+export { default as IDropdownDivider } from '@inkline/inkline/src/components/DropdownDivider';
+export { default as IDropdownItem } from '@inkline/inkline/src/components/DropdownItem';
+export { default as IDropdownMenu } from '@inkline/inkline/src/components/DropdownMenu';
+export { default as IHamburgerMenu } from '@inkline/inkline/src/components/HamburgerMenu';
+export { default as IHeader } from '@inkline/inkline/src/components/Header';
+export { default as IIcon } from '@inkline/inkline/src/components/Icon';
+export { default as ILayout } from '@inkline/inkline/src/components/Layout';
+export { default as ILayoutAside } from '@inkline/inkline/src/components/LayoutAside';
+export { default as ILayoutContent } from '@inkline/inkline/src/components/LayoutContent';
+export { default as ILayoutFooter } from '@inkline/inkline/src/components/LayoutFooter';
+export { default as ILayoutHeader } from '@inkline/inkline/src/components/LayoutHeader';
+export { default as IListGroup } from '@inkline/inkline/src/components/ListGroup';
+export { default as IListGroupItem } from '@inkline/inkline/src/components/ListGroupItem';
+export { default as ILoader } from '@inkline/inkline/src/components/Loader';
+export { default as IMedia } from '@inkline/inkline/src/components/Media';
+export { default as IModal } from '@inkline/inkline/src/components/Modal';
+export { default as INav } from '@inkline/inkline/src/components/Nav';
+export { default as INavItem } from '@inkline/inkline/src/components/NavItem';
+export { default as INavbar } from '@inkline/inkline/src/components/Navbar';
+export { default as INavbarBrand } from '@inkline/inkline/src/components/NavbarBrand';
+export { default as INavbarItems } from '@inkline/inkline/src/components/NavbarItems';
+export { default as IRow } from '@inkline/inkline/src/components/Row';
+export { default as IPagination } from '@inkline/inkline/src/components/Pagination';
+export { default as IPopover } from '@inkline/inkline/src/components/Popover';
+export { default as IProgress } from '@inkline/inkline/src/components/Progress';
+export { default as IProgressBar } from '@inkline/inkline/src/components/ProgressBar';
+export { default as ITable } from '@inkline/inkline/src/components/Table';
+export { default as ITooltip } from '@inkline/inkline/src/components/Tooltip';
 
-import ICheckbox from '@inkline/inkline/src/components/Checkbox';
-import ICheckboxButton from '@inkline/inkline/src/components/CheckboxButton';
-import ICheckboxGroup from '@inkline/inkline/src/components/CheckboxGroup';
-import ICheckboxButtonGroup from '@inkline/inkline/src/components/CheckboxButtonGroup';
-import IInput from '@inkline/inkline/src/components/Input';
-import IForm from '@inkline/inkline/src/components/Form';
-import IFormGroup from '@inkline/inkline/src/components/FormGroup';
-import IFormLabel from '@inkline/inkline/src/components/FormLabel';
-import IInputNumber from '@inkline/inkline/src/components/InputNumber';
-import IRadio from '@inkline/inkline/src/components/Radio';
-import IRadioButton from '@inkline/inkline/src/components/RadioButton';
-import IRadioGroup from '@inkline/inkline/src/components/RadioGroup';
-import IRadioButtonGroup from '@inkline/inkline/src/components/RadioButtonGroup';
-import ISelect from '@inkline/inkline/src/components/Select';
-import ISelectOption from '@inkline/inkline/src/components/SelectOption';
-import ITextarea from '@inkline/inkline/src/components/Textarea';
-
-export {
-    IAlert,
-    IBadge,
-    IBreadcrumb,
-    IBreadcrumbItem,
-    IButton,
-    IButtonGroup,
-    ICard,
-    ICollapsible,
-    ICollapsibleItem,
-    IColumn,
-    IContainer,
-    IDropdown,
-    IDropdownDivider,
-    IDropdownItem,
-    IDropdownMenu,
-    IHamburgerMenu,
-    IHeader,
-    IIcon,
-    ILayout,
-    ILayoutAside,
-    ILayoutContent,
-    ILayoutFooter,
-    ILayoutHeader,
-    IListGroup,
-    IListGroupItem,
-    ILoader,
-    IMedia,
-    IModal,
-    INav,
-    INavItem,
-    INavbar,
-    INavbarBrand,
-    INavbarItems,
-    IRow,
-    IPagination,
-    IPopover,
-    IProgress,
-    IProgressBar,
-    ITable,
-    ITooltip,
-
-    ICheckbox,
-    ICheckboxButton,
-    ICheckboxGroup,
-    ICheckboxButtonGroup,
-    IInput,
-    IForm,
-    IFormGroup,
-    IFormLabel,
-    IInputNumber,
-    IRadio,
-    IRadioButton,
-    IRadioGroup,
-    IRadioButtonGroup,
-    ISelect,
-    ISelectOption,
-    ITextarea
-};
+export { default as ICheckbox } from '@inkline/inkline/src/components/Checkbox';
+export { default as ICheckboxButton } from '@inkline/inkline/src/components/CheckboxButton';
+export { default as ICheckboxGroup } from '@inkline/inkline/src/components/CheckboxGroup';
+export { default as ICheckboxButtonGroup } from '@inkline/inkline/src/components/CheckboxButtonGroup';
+export { default as IInput } from '@inkline/inkline/src/components/Input';
+export { default as IForm } from '@inkline/inkline/src/components/Form';
+export { default as IFormGroup } from '@inkline/inkline/src/components/FormGroup';
+export { default as IFormLabel } from '@inkline/inkline/src/components/FormLabel';
+export { default as IInputNumber } from '@inkline/inkline/src/components/InputNumber';
+export { default as IRadio } from '@inkline/inkline/src/components/Radio';
+export { default as IRadioButton } from '@inkline/inkline/src/components/RadioButton';
+export { default as IRadioGroup } from '@inkline/inkline/src/components/RadioGroup';
+export { default as IRadioButtonGroup } from '@inkline/inkline/src/components/RadioButtonGroup';
+export { default as ISelect } from '@inkline/inkline/src/components/Select';
+export { default as ISelectOption } from '@inkline/inkline/src/components/SelectOption';
+export { default as ITextarea } from '@inkline/inkline/src/components/Textarea';
