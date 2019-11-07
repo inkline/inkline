cd2da22cb04270ea349556d311e7422095daf6ec
Alex Grozav <alex@grozav.com>
2019-11-07 15:04:03 +0200

    Added back bundle optimization changes.

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,12 @@ module.exports = {
         }
     },
     chainWebpack: config => {
+        config.optimization.delete("splitChunks");
+        config.optimization.removeAvailableModules(false);
+        config.optimization.concatenateModules(false);
+        config.optimization.providedExports(false);
+        config.optimization.usedExports(false);
+
         config.resolve.alias.set("@inkline/inkline", __dirname).end();
 
         // config
