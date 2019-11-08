c870ecc6bbc13d0e9c4b54e0a7eeeeb92612d1a2
Alex Grozav <alex@grozav.com>
2019-11-08 16:38:34 +0200

    Initial sidebar docs. Work in progress.

---
+++ packages/inkline.io/pages/docs/components/sidebar.vue
@@ -0,0 +1,27 @@
+<template lang="md" src="./sidebar.md" />
+<script>
+import View from '@components/docs/View';
+import { head } from "@helpers/head";
+import { ISidebar, INav, INavItem, ILayout, ILayoutAside, ILayoutContent } from "@inkline/inkline/src/components";
+
+export default {
+    name: 'SidebarView',
+    layout: 'documentation',
+    extends: View,
+    head: head('docs-components-sidebar'),
+    components: {
+        ISidebar,
+        // ISidebarItems,
+        INav,
+        INavItem,
+        ILayout,
+        ILayoutAside,
+        ILayoutContent
+    },
+    data() {
+        return {
+            collapsed: false
+        };
+    }
+};
+</script>
