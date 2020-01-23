fa816df369033383c13a33387b3a60ad5f5fac53
Alex Grozav <alex@grozav.com>
2020-01-23 12:38:14 +0200

    Finished datatable docs. Added api page and finished expanding page.

---
+++ packages/inkline.io/pages/docs/components/dashboard/datatable/api.vue
@@ -0,0 +1,22 @@
+<template lang="md" src="./api.md" />
+<script>
+import View from '@components/docs/View';
+import { head } from "@helpers/head";
+import { generateDataRows } from "@helpers/generateDataRows";
+import { IDatatable, IAlert, IIcon } from '@inkline/inkline/src';
+
+export default {
+    extends: View,
+    name: 'DataTableApiView',
+    layout: 'documentation',
+    head: head('docs-components-dashboard-datatable-api'),
+    components: {
+        IDatatable,
+        IAlert,
+        IIcon
+    },
+    data () {
+        return {};
+    }
+};
+</script>
