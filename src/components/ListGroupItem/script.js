bca65b083501b2eb67548a29887fb8f5c6615c8b
Alex Grozav <alex@grozav.com>
2020-01-27 17:43:09 +0200

    Updated breadcrumb template. Removed disabled from list group item.

--- src/components/ListGroupItem/script.js
+++ src/components/ListGroupItem/script.js
@@ -3,16 +3,12 @@ import Linkable from '../Linkable';
 import AttributesProviderMixin from '@inkline/inkline/src/mixins/components/providers/AttributesProviderMixin';
 import ClassesProviderMixin from '@inkline/inkline/src/mixins/components/providers/ClassesProviderMixin';
 
-import DisabledPropertyMixin from '@inkline/inkline/src/mixins/components/properties/DisabledPropertyMixin';
-
 export default {
     name: 'IListGroupItem',
     extends: Linkable,
     mixins: [
         AttributesProviderMixin,
-        ClassesProviderMixin,
-
-        DisabledPropertyMixin
+        ClassesProviderMixin
     ],
     props: {
         tag: {
