66387c6f2c07ed011997a1684a2ad3ff83b9d784
Alex Grozav <alex@grozav.com>
2020-01-29 00:16:05 +0200

    Made form prototype install by default.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 export * from '@inkline/inkline/src/components';
+import { form } from '@inkline/inkline/src/prototypes/form';
+
 export const Inkline = {
     install(Vue, options = {}) {
+        Vue.prototype.$inkline = { form };
+
         for (const componentIndex in options.components) {
             Vue.component(options.components[componentIndex].name, options.components[componentIndex]);
         }
