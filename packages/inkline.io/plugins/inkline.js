5633929917cb54725346904689b5858542f4aa97
Alex Grozav <alex@grozav.com>
2020-01-29 07:51:58 +0200

    Removed validation imports.

--- packages/inkline.io/plugins/inkline.js
+++ packages/inkline.io/plugins/inkline.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import { Inkline } from '@inkline/inkline/src/index';
-import InklineValidation from '@inkline/inkline/src/validation';
 import '@inkline/inkline/index.scss';
 
 Vue.use(Inkline);
-Vue.use(InklineValidation);
