076ed710a4f3511798d27a85558a97331230cf51
Alex Grozav <alex@grozav.com>
2020-02-02 13:52:43 +0200

    Updated docs dependencies.

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     presets: [
         "@vue/cli-plugin-babel/preset",
-        "@nuxt/babel-preset-app"
+        [
+            "@nuxt/babel-preset-app", {
+                corejs: { version: 3 }
+            }
+        ]
     ],
     plugins: [
         "@babel/plugin-proposal-class-properties"
