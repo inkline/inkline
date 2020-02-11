f92b98b869651b658b72c3f6fe5dc1a211a46ab1
Alex Grozav <alex@grozav.com>
2020-02-11 13:51:53 +0200

    Added v-click-outside directive handler and collapseOnClickOutside property for Navbar.

--- packages/inkline/src/components/Navbar/script.js
+++ packages/inkline/src/components/Navbar/script.js
@@ -7,6 +7,8 @@ import AttributesProviderMixin from '@inkline/inkline/src/mixins/components/prov
 import ClassesProviderMixin from '@inkline/inkline/src/mixins/components/providers/ClassesProviderMixin';
 import CollapsibleProviderMixin from '@inkline/inkline/src/mixins/components/providers/CollapsibleProviderMixin';
 
+import ClickOutside from '@inkline/inkline/src/directives/click-outside';
+
 import SizePropertyMixin from '@inkline/inkline/src/mixins/components/properties/SizePropertyMixin';
 import VariantPropertyMixin from '@inkline/inkline/src/mixins/components/properties/VariantPropertyMixin';
 
@@ -27,8 +29,15 @@ export default {
         IColumn,
         IHamburgerMenu
     },
+    directives: {
+        ClickOutside
+    },
     props: {
         collapseOnClick: {
+            type: Boolean,
+            default: true
+        },
+        collapseOnClickOutside: {
             type: Boolean,
             default: true
         },
@@ -42,16 +51,21 @@ export default {
         }
     },
     methods: {
-        onItemClick() {
+        onClickItem() {
             if (this.collapseOnClick && this.collapsed) {
+                this.setCollapse(false);
+            }
+        },
+        onClickOutside() {
+            if (this.collapseOnClickOutside && this.collapsed) {
                 this.setCollapse(false);
             }
         }
     },
     created() {
-        this.$on('item-click', this.onItemClick);
+        this.$on('item-click', this.onClickItem);
     },
     beforeDestroy() {
-        this.$off('item-click', this.onItemClick);
+        this.$off('item-click', this.onClickItem);
     }
 };
