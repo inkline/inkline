ea2f6c180f10d42ede154c34526e79b6a18fbfb6
Alex Grozav <alex@grozav.com>
2020-02-20 18:49:42 +0200

    Added vue-cli-plugin testing workflow.

--- packages/vue-cli-plugin/tests/app/src/main.default.js
+++ packages/vue-cli-plugin/tests/app/src/main.default.js
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '@inkline/inkline/dist/inkline.css'
+import Inkline from '@inkline/inkline'
+
+Vue.use(Inkline);
 
 Vue.config.productionTip = false;
 
