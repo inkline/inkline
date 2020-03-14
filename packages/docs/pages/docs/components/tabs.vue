47fde255aa0c46b7eb14ff7d56c6cc48d456fa92
Alex Grozav <alex@grozav.com>
2020-03-14 14:16:38 +0200

    Added documentation for tab components.

---
+++ packages/docs/pages/docs/components/tabs.vue
@@ -0,0 +1,25 @@
+<template lang="md" src="./tabs.md" />
+<script>
+import View from '@components/docs/View';
+import { head } from "@helpers/head";
+import { ITabs, ITab, IAlert, IIcon, IButton } from "@inkline/inkline/src/components";
+
+export default {
+    name: 'TabsView',
+    layout: 'documentation',
+    extends: View,
+    head: head('docs-components-tabs'),
+    components: {
+        ITabs,
+        ITab,
+        IAlert,
+        IIcon,
+        IButton
+    },
+    data() {
+        return {
+            active: 'tab-2'
+        };
+    },
+};
+</script>
