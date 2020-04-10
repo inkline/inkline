30f4edd761b8ca1747cf3900dfa7493e37c61f3e
Alex Grozav <alex@grozav.com>
2020-04-10 13:08:48 +0300

    Added APITable and APITableRow components.

--- packages/docs/components/docs/View/script.js
+++ packages/docs/components/docs/View/script.js
@@ -1,10 +1,14 @@
 import { ITable } from "@inkline/inkline/src/index";
 import IApiPreview from '../ApiPreview';
 import ICodePreview from '../CodePreview';
+import ApiTable from '../ApiTable';
+import ApiTableRow from '../ApiTableRow';
 
 export default {
     name: 'View',
     components: {
+        ApiTable,
+        ApiTableRow,
         ICodePreview,
         IApiPreview,
         ITable
