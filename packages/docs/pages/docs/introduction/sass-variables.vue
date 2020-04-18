e2680d49498ef56dab272452d0a4cba40f7c79c6
Alex Grozav <alex@grozav.com>
2020-04-18 15:56:30 +0300

    Added common sass files documentation.

---
+++ packages/docs/pages/docs/introduction/sass-variables.vue
@@ -0,0 +1,17 @@
+<template lang="md" src="./sass-variables.md" />
+<script>
+import View from '@components/docs/View';
+import { head } from "@helpers/head";
+import { IAlert, IIcon } from "@inkline/inkline/src/components";
+
+export default {
+    name: 'SassVariablesView',
+    layout: 'documentation',
+    extends: View,
+    head: head('docs-introduction-sass-variables'),
+    components: {
+        IAlert,
+        IIcon
+    }
+};
+</script>
