4275717a46890e014e0c802ca05da428fd6befb2
Alex Grozav <alex@grozav.com>
2020-04-24 20:12:39 +0300

    Updated src/inkline.js exports.

--- packages/inkline/src/inkline.js
+++ packages/inkline/src/inkline.js
@@ -2,7 +2,8 @@ import '@inkline/inkline/src/inkline.scss';
 import { Inkline } from '@inkline/inkline/src/plugin';
 import * as components from '@inkline/inkline/src/components';
 
-export * from '@inkline/inkline/src/index';
+export { Inkline } from '@inkline/inkline/src/plugin';
+export * from '@inkline/inkline/src/components';
 
 const install = Inkline.install;
 Inkline.install = (Vue, options = {}) => install.call(Inkline, Vue, { components, ...options });
