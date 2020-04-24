85e3f1f20cd75535e5ebc75eb68efde48a4a2c0c
Alex Grozav <alex@grozav.com>
2020-04-24 20:00:07 +0300

    Renamed all component tests from ComponentName.spec.js to IComponentName.spec.js.

--- packages/inkline/tests/components/node.js
+++ packages/inkline/tests/components/node.js
@@ -8,7 +8,7 @@ const folders = glob.sync('*')
 folders.forEach((folder) => {
     if (folder.indexOf('.spec.js') === -1) { return }
 
-    // child_process.execSync(`git mv ${folder} I${folder}`)
+    child_process.execSync(`git mv ${folder} I${folder}`)
 
-    fs.renameSync(folder, folder.replace(/^I/, ''))
+    // fs.renameSync(folder, folder.replace(/^I/, ''))
 });
