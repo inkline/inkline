8e076991b44a1a933b494e7d53b2e9cc638bbeae
Alex Grozav <alex@grozav.com>
2020-04-27 01:02:25 +0300

    Removed compiled js file.

--- packages/vue-cli-plugin/tests/app-ts/.eslintrc.js
+++ packages/vue-cli-plugin/tests/app-ts/.eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
+    root: true,
+    env: {
     node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'indent': 'off'
-  }
-}
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'indent': 'off'
+    }
+};
