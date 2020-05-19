4e453033715c0b2e7925b9d6f28ba698595f329d
Alex Grozav <alex@grozav.com>
2020-05-20 00:58:22 +0300

    Added parent form group registration to schema provider.

---
+++ packages/inkline/src/mixins/providers/SchemaProviderMixin.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: {
+        schema: {
+            type: Object,
+            default: () => null
+        },
+    },
+    mounted() {
+        if (this.schema) {
+            if (this.parentForm) {
+                this.parentForm.add(this);
+            }
+
+            if (this.parentFormGroup) {
+                this.$set(this.parentFormGroup, 'inputSchema', this.schema);
+            }
+        }
+    },
+    destroyed() {
+        if (this.schema && this.parentForm) {
+            this.parentForm.remove(this);
+        }
+    }
+};
+</script>
