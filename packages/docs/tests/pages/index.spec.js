2bf4cd4fb2198314fab5279eb1d23e4653837caf
Alex Grozav <alex@grozav.com>
2020-06-05 17:18:18 +0300

    Updated index page snapshot.

--- packages/docs/tests/pages/index.spec.js
+++ packages/docs/tests/pages/index.spec.js
@@ -1,11 +1,15 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import IndexView from '@/pages/index.vue';
 
 describe('IndexView', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallowMount(IndexView);
+        wrapper = shallowMount(IndexView, {
+            stubs: {
+                NuxtLink: RouterLinkStub
+            }
+        });
     });
 
     it('is should render correctly', () => {
