1880b4875df9017b6e5b2417fb92510b2c8cceec
Alex Grozav <alex@grozav.com>
2020-06-15 10:01:00 +0300

    Added plugin options page.

---
+++ packages/docs/pages/docs/introduction/plugin-options.vue
@@ -0,0 +1,18 @@
+<template lang="md" src="./plugin-options.md" />
+<script>
+import View from '@components/docs/View';
+import { head } from "@helpers/head";
+import { IAlert, IIcon, ICard } from "@inkline/inkline/src/components";
+
+export default {
+    name: 'PluginOptionsView',
+    layout: 'documentation',
+    extends: View,
+    head: head(),
+    components: {
+        IAlert,
+        IIcon,
+        ICard
+    }
+};
+</script>
