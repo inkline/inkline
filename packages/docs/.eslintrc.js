2dfb1cca7018f5c0d0fe983e049c3765b0381533
Alex Grozav <alex@grozav.com>
2020-06-19 21:31:16 +0300

    Updated linting rules.

--- packages/docs/.eslintrc.js
+++ packages/docs/.eslintrc.js
@@ -8,12 +8,23 @@ module.exports = {
         parser: 'babel-eslint'
     },
     extends: [
-        "plugin:vue/recommended"
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
     ],
     rules: {
+        'semi': ['warn', 'always'],
         'indent': ['warn', 4],
+        'vue/html-indent': ['warn', 4],
         'no-console': 'off',
         'no-prototype-builtins': 'off',
-        'vue/order-in-components': 'off'
+        'vue/order-in-components': 'off',
+        'import/order': 'off',
+        'space-before-function-paren': 'off',
+        'arrow-parens': ['warn', 'always'],
+        'vue/max-attributes-per-line': 'off',
+        'vue/no-use-v-if-with-v-for': 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'comma-dangle': 'warn',
+        'no-multiple-empty-lines': 'warn'
     }
 };
