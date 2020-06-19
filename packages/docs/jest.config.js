e1bca0fa99fe4fbd4489ff3e9aa7c4b385951620
Alex Grozav <alex@grozav.com>
2020-06-19 21:32:07 +0300

    Updated jest config linting.

--- packages/docs/jest.config.js
+++ packages/docs/jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    moduleFileExtensions: ["js", "jsx", "json", "vue"],
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
     collectCoverage: true,
-    coverageReporters: ["json", "html", "lcov"],
-    coverageDirectory: "<rootDir>/coverage",
+    coverageReporters: ['json', 'html', 'lcov'],
+    coverageDirectory: '<rootDir>/coverage',
     collectCoverageFrom: [
-        "<rootDir>/{components,helpers}/**/*.{js,vue}",
+        '<rootDir>/{components,helpers}/**/*.{js,vue}'
         // "<rootDir>/layouts/**/*.vue",
         // "<rootDir>/pages/**/*.vue",
         // "!<rootDir>/node_modules/**"
     ],
     transform: {
-        "^.+\\.vue$": "vue-jest",
-        ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
-        "^.+\\.jsx?$": "babel-jest"
+        '^.+\\.vue$': 'vue-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+\\.jsx?$': 'babel-jest'
     },
     transformIgnorePatterns: [
-        "node_modules/(?!@inkline/inkline|vue-github-button)"
+        'node_modules/(?!@inkline/inkline|vue-github-button)'
     ],
     moduleNameMapper: {
         '^@helpers/(.*)$': '<rootDir>/helpers/$1',
@@ -25,10 +25,10 @@ module.exports = {
         '^@inkline/inkline/(.*)$': '<rootDir>/node_modules/@inkline/inkline/$1',
         '^vue$': 'vue/dist/vue.common.js'
     },
-    snapshotSerializers: ["jest-serializer-vue"],
+    snapshotSerializers: ['jest-serializer-vue'],
     testMatch: [
-        "**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+        '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
     ],
-    testURL: "http://localhost/",
+    testURL: 'http://localhost/',
     verbose: true
 };
