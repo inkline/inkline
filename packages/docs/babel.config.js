3a479cfc69b482e00115a2ba5d9f1583619f02a0
Alex Grozav <alex@grozav.com>
2020-06-19 21:33:02 +0300

    Updated babel config linting.

--- packages/docs/babel.config.js
+++ packages/docs/babel.config.js
@@ -1,14 +1,14 @@
 module.exports = {
     presets: [
-        "@vue/cli-plugin-babel/preset",
+        '@vue/cli-plugin-babel/preset',
         [
-            "@nuxt/babel-preset-app", {
+            '@nuxt/babel-preset-app', {
                 corejs: { version: 3 }
             }
         ]
     ],
     plugins: [
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-transform-runtime"
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-runtime'
     ]
 };
