ae69381350d3c062f96d2bbe38e9a07bb77bd3d0
Eternal-Rise <my_dawn@ukr.net>
2020-06-28 15:38:00 +0300

    fix tests after migrate to Popper 2

--- packages/inkline/tests/jest/setup.js
+++ packages/inkline/tests/jest/setup.js
@@ -13,25 +13,3 @@
         set(v) { value = v; }
     }))(object[property]));
 });
-
-jest.mock('popper.js', () => {
-    const PopperJS = jest.requireActual('popper.js');
-
-    return class {
-        static placements = PopperJS.placements;
-
-        constructor() {
-            return {
-                update: () => {},
-                destroy: () => {},
-                scheduleUpdate: () => {},
-                popper: {
-                    getAttribute() {
-                        return 'top';
-                    },
-                    style: {}
-                }
-            };
-        }
-    };
-});
