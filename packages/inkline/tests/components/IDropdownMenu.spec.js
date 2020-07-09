7620f218c406db669b482597b122110db9cf8db0
Alex Grozav <alex@grozav.com>
2020-07-09 18:07:11 +0300

    Updated arrow offset in tests.

--- packages/inkline/tests/components/IDropdownMenu.spec.js
+++ packages/inkline/tests/components/IDropdownMenu.spec.js
@@ -39,7 +39,7 @@ describe('Components', () => {
             describe('arrowOffset', () => {
                 it('should be defined', () => {
                     expect(wrapper.vm.arrowOffset).toBeDefined();
-                    expect(wrapper.vm.arrowOffset).toEqual(0);
+                    expect(wrapper.vm.arrowOffset).toEqual(10);
                 });
             });
         });
