3bba01e8f6843e38b6b6882dae67d1e0a8fb0be0
Alex Grozav <alex@grozav.com>
2021-03-14 18:20:12 +0200

    Updated type definitions.

---
+++ packages/inkline3/src/components/ICollapsible/components/index.d.ts
@@ -0,0 +1,1 @@
+export { default as ICollapsibleItem } from './ICollapsibleItem/index.vue';
---
+++ packages/inkline3/src/components/IPagination/index.d.ts
@@ -0,0 +1,5 @@
+import { Component } from 'vue';
+
+declare const IPagination: Component;
+
+export default IPagination;
