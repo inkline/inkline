ebe5578e9e8e2caa047eeffae5c4915fd43fb032
Alex Grozav <alex@grozav.com>
2021-04-30 11:48:26 +0300

    docs(universal icons): Updated integration examples.

--- packages/inkline3/src/components/IIcon/examples/integrations.vue
+++ packages/inkline3/src/components/IIcon/examples/integrations.vue
@@ -9,7 +9,7 @@
                     Prefix
                 </th>
                 <th>
-                    Path
+                    Import Path
                 </th>
                 <th>
                     License
@@ -19,7 +19,7 @@
         <tbody>
             <tr>
                 <th>
-                    Bootstrap Icons
+                    <a href="https://icons.getbootstrap.com/" rel="nofollow" target="_blank">Bootstrap Icons</a>
                 </th>
                 <td>
                     <code>bi</code>
@@ -28,19 +28,19 @@
                     <code>@inkline/inkline/src/icons/bootstrap</code>
                 </td>
                 <td>
-                    <a href="https://github.com/twbs/icons/blob/main/LICENSE.md" rel="nofollow noreferrer" target="_blank">MIT</a>
+                    <a href="https://github.com/twbs/icons/blob/main/LICENSE.md" rel="nofollow" target="_blank">MIT</a>
                 </td>
             </tr>
             <tr>
                 <th>
-                    CoreUI Icons
+                    <a href="https://icons.coreui.io/icons/" rel="nofollow" target="_blank">CoreUI Icons</a>
                 </th>
                 <td></td>
                 <td>
                     <code>@inkline/inkline/src/icons/coreui</code>
                 </td>
                 <td>
-                    <a href="https://github.com/coreui/coreui-icons/blob/master/LICENSE" rel="nofollow noreferrer" target="_blank">CC BY 4.0 License</a>
+                    <a href="https://github.com/coreui/coreui-icons/blob/master/LICENSE" rel="nofollow" target="_blank">CC BY 4.0</a>
                 </td>
             </tr>
             <tr>
@@ -81,14 +81,14 @@
             </tr>
             <tr>
                 <th>
-                    Flag Icons
+                    <a href="https://flagicons.lipis.dev/" rel="nofollow" target="_blank">Flag Icons</a>
                 </th>
                 <td></td>
                 <td>
                     <code>@inkline/inkline/src/icons/flag</code>
                 </td>
                 <td>
-                    <a href="https://github.com/lipis/flag-icon-css/blob/master/LICENSE" rel="nofollow noreferrer" target="_blank">MIT</a>
+                    <a href="https://github.com/lipis/flag-icon-css/blob/master/LICENSE" rel="nofollow" target="_blank">MIT</a>
                 </td>
             </tr>
             <tr>
@@ -112,6 +112,104 @@
                 </td>
                 <td>
                     <code>@inkline/inkline/src/icons/flag/4x3</code>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>
+                    <a href="https://github.com/icons8/flat-color-icons" rel="nofollow" target="_blank">Flat Color Icons</a>
+                </th>
+                <td>
+                    <code>fc</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/flat-color</code>
+                </td>
+                <td>
+                    <a href="https://github.com/icons8/flat-color-icons/blob/master/LICENSE.md" rel="nofollow" target="_blank">MIT / Good Boy</a>
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <a href="https://fontawesome.com/" rel="nofollow" target="_blank">FontAwesome Icons</a>
+                </th>
+                <td></td>
+                <td>
+                    <code>@inkline/inkline/src/icons/fontawesome</code>
+                </td>
+                <td>
+                    <a href="https://github.com/FortAwesome/Font-Awesome/blob/master/LICENSE.txt" rel="nofollow" target="_blank">CC BY 4.0</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    FontAwesome Brand Icons
+                </td>
+                <td>
+                    <code>fab</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/fontawesome/brand</code>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    FontAwesome Regular Icons
+                </td>
+                <td>
+                    <code>far</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/fontawesome/regular</code>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    FontAwesome Solid Icons
+                </td>
+                <td>
+                    <code>fas</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/fontawesome/solid</code>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>
+                    <a href="https://heroicons.com/" rel="nofollow" target="_blank">Heroicons</a>
+                </th>
+                <td></td>
+                <td>
+                    <code>@inkline/inkline/src/icons/heroicons</code>
+                </td>
+                <td>
+                    <a href="https://github.com/tailwindlabs/heroicons/blob/master/LICENSE" rel="nofollow" target="_blank">MIT</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Heroicons Outline Icons
+                </td>
+                <td>
+                    <code>hio</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/heroicons/outline</code>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>
+                    Heroicons Solid Icons
+                </td>
+                <td>
+                    <code>his</code>
+                </td>
+                <td>
+                    <code>@inkline/inkline/src/icons/heroicons/solid</code>
                 </td>
                 <td></td>
             </tr>
