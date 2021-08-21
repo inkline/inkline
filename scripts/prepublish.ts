72c90f2e349cf6b7c7e18e8a7312fa9eaedfc237
Alex Grozav <alex@grozav.com>
2021-08-21 11:33:15 +0300

    ci: Added prepublish script. Removed irrelevant files.

---
+++ scripts/prepublish.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import fse from 'fs-extra';
+import path from 'path';
+
+const packageFolder = path.resolve(__dirname, '..', '.npm');
+
+const remove = [
+    {
+        from: packageFolder,
+        dir: true
+    }
+];
+
+const mkdir = [
+    {
+        from: packageFolder
+    }
+];
+
+const copy = [
+    {
+        from: path.resolve(__dirname, '..', 'lib'),
+        to: packageFolder
+    },
+    {
+        from: path.resolve(__dirname, '..', 'dist'),
+        to: path.resolve(packageFolder, 'dist')
+    },
+    {
+        from: path.resolve(__dirname, '..', 'package.json'),
+        to: path.resolve(packageFolder, 'package.json')
+    },
+    {
+        from: path.resolve(__dirname, '..', 'README.md'),
+        to: path.resolve(packageFolder, 'README.md')
+    },
+    {
+        from: path.resolve(__dirname, '..', 'LICENSE'),
+        to: path.resolve(packageFolder, 'LICENSE')
+    }
+];
+
+(async () => {
+    for (const entry of remove) {
+        try {
+            await fs.promises[entry.dir ? 'rmdir' : 'rm'](entry.from, { recursive: true });
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    for (const entry of mkdir) {
+        try {
+            await fs.promises.mkdir(entry.from, { recursive: true });
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    for (const entry of copy) {
+        try {
+            await fse.copy(entry.from, entry.to);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+})();
