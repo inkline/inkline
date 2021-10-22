e526601584a73228bad489bbfe2270b21b11bd3c
Alex Grozav <alex@grozav.com>
2021-10-22 08:49:22 +0300

    refactor: Moved grid documentation and examples to docs/core/grid folder.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from '@inkline/inkline/components';
 export * from '@inkline/inkline/composition-api';
 export * from '@inkline/inkline/i18n';
 export * from '@inkline/inkline/validation';
+export * from '@inkline/inkline/helpers';
