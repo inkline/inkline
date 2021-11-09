06c672030d9dd76450e126124b0bc6c19353eb55
Alex Grozav <alex@grozav.com>
2021-11-09 09:49:56 +0200

    ci: Extracted common vite config to be used with storybook.

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,14 @@
+import commonConfig from './vite.common.config';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import vue from '@vitejs/plugin-vue';
-import markdown from 'vite-plugin-md';
 
-function html() {
-    return {
-        name: 'html',
-        transform (code, id) {
-            if (/\.html$/.test(id)) {
-                return `export default ${JSON.stringify(code)}`;
-            }
-        }
-    };
-}
-
-// https://vitejs.dev/config/
+/**
+ * Vite configuration for library build
+ *
+ * @doc https://vitejs.dev/config/
+ */
 export default defineConfig({
-    plugins: [
-        vue({
-            include: [
-                /\.vue$/,
-                /\.md$/
-            ]
-        }),
-        markdown(),
-        html()
-    ],
-    resolve: {
-        alias: [
-            {
-                find: /^@inkline\/inkline\//,
-                replacement: `${resolve(__dirname)}/src/`
-            }
-        ]
-    },
+    ...commonConfig,
     build: {
         lib: {
             entry: resolve(__dirname, 'src', 'inkline.ts'),
@@ -52,13 +27,5 @@ export default defineConfig({
                 }
             }
         }
-    },
-    optimizeDeps: {
-        exclude: [
-            'dist',
-            'lib',
-            'package',
-            'coverage'
-        ]
     }
 });
