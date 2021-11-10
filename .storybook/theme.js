a8442469ffe34032899d018270fedcdb3cddf58b
Alex Grozav <alex@grozav.com>
2021-11-10 09:00:41 +0200

    storybook: Added inkline branding to storybook.

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import logo from './public/logo.png';
+
+export default create({
+    base: 'light',
+    brandTitle: 'inkline',
+    brandUrl: 'https://inkline.io',
+    brandImage: logo
+});
