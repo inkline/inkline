a37b688977ebd2ad79d5c9e39bb34847cbb2133f
Alex Grozav <alex@grozav.com>
2021-12-14 11:19:17 +0200

    feat: Updated storybook to latest version.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,63 +1,37 @@
-const { resolve } = require("path");
-
-function webpackInjectPublicPath(webpackConfig, publicPath) {
-    const configHtmlWebPackPlugin = webpackConfig.plugins.find(
-        (plugin) => plugin.constructor.name === 'HtmlWebpackPlugin',
-    );
-    const templateParameters = configHtmlWebPackPlugin.options.templateParameters
-
-    configHtmlWebPackPlugin.options.publicPath = publicPath;
-    webpackConfig.output.publicPath = publicPath;
-
-    configHtmlWebPackPlugin.options.templateParameters = (compilation, files, options) => {
-        const result = templateParameters(compilation, files, options);
-        result.globals.PREVIEW_URL = `${publicPath}iframe.html`;
-        return result;
-    }
-}
+const path = require('path');
+const webpackConfig = require('../webpack.config.js');
+const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
+    stories: [
+        '../src/components/IAlert/**/*.stories.mdx',
+        '../src/components/IAlert/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-postcss',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-a11y'
+    ],
     framework: '@storybook/vue3',
     features: {
         storyStoreV7: true,
     },
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-a11y"
-    ],
-    core: {
-        builder: "storybook-builder-vite"
-    },
-    async viteFinal(config, { configType }) {
-        config.resolve.alias = [
-            {
-                find: /^@inkline\/inkline\//,
-                replacement: `${resolve(__dirname)}/../src/`
-            },
-            {
-                find: /^~@inkline\/icons/,
-                replacement: '@inkline/icons'
-            }
+    webpackFinal: async (config, { configType }) => {
+        config.resolve.alias = webpackConfig.resolve.alias;
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: path.resolve(__dirname, '../'),
+        });
+
+        config.resolve.plugins = [
+            ...(config.resolve.plugins || []),
+            new tsconfigPathsPlugin({
+                extensions: config.resolve.extensions,
+            }),
         ];
 
-        if (configType === 'DEVELOPMENT') {
-            config.server.force = false;
-        } else {
-            config.base = "/storybook/";
-        }
-
         return config;
-    },
-    managerWebpack: async (config, { configType }) => {
-        if (configType !== 'DEVELOPMENT') {
-            webpackInjectPublicPath(config, '/storybook/');
-        }
-
-        return config;
-    },
+    }
 }
