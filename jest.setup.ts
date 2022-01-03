cfa8d09f9c08d37a6aa5f0cb93bc64895a7ccda9
Alex Grozav <alex@grozav.com>
2022-01-03 18:39:47 +0200

    feat: Updated icons imports

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,21 +1,15 @@
 import '@testing-library/jest-dom';
 import { inklineGlobals, defaultOptions as defaultPrototypeOptions, createPrototype } from './src/plugin';
-import { defineComponent } from 'vue';
 import { localStorageMock } from './src/__mocks__/localStorageMock';
 import { matchMediaMock } from './src/__mocks__/matchMediaMock';
 
 jest.mock('@inkline/icons', () => ({
     InklineIcons: {
         add: jest.fn()
-    }
-}));
-jest.mock('@inkline/icons/packs/inkline', () => ({}));
-jest.mock('@inkline/icons/components/index', () => ({}));
-
-jest.mock('@inkline/icons/components', () => ({
-    IIcon: defineComponent({
+    },
+    IIcon: {
         template: '<i />'
-    })
+    }
 }));
 
 inklineGlobals.prototype = createPrototype(defaultPrototypeOptions);
