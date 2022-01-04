de891f6a9a299829cf6cfbb8947e94f34a8ad7d6
Alex Grozav <alex@grozav.com>
2022-01-04 18:07:47 +0200

    fix: Fixed tests. Moved scripts to shelljs.

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,20 +3,11 @@ import { inklineGlobals, defaultOptions as defaultPrototypeOptions, createProtot
 import { localStorageMock } from './src/__mocks__/localStorageMock';
 import { matchMediaMock } from './src/__mocks__/matchMediaMock';
 
-jest.mock('@inkline/icons', () => ({
-    InklineIcons: {
-        add: jest.fn()
-    },
-    IIcon: {
-        template: '<i />'
-    }
-}));
-
 inklineGlobals.prototype = createPrototype(defaultPrototypeOptions);
 
 Object.defineProperty(global.window, 'localStorage', { value: localStorageMock });
 
 Object.defineProperty(global.window, 'matchMedia', {
     writable: true,
-    value: jest.fn().mockImplementation(matchMediaMock),
+    value: jest.fn().mockImplementation(matchMediaMock)
 });
--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,6 +1,7 @@
+/* eslint-disable quotes */
+
 import shell from 'shelljs';
 import path from 'path';
-import {packageFolderPath} from "./config";
 
 /**
  * Change directory to root
@@ -12,21 +13,34 @@ shell.cd(path.resolve(__dirname, '..'));
  * Copy files from src to lib
  */
 
-shell.exec("bash -c 'rsync -am --include=\"*.\"{vue,scss,html,js,md,d.ts} --include=\"*/\" --exclude=\"*\" src/ lib/'");
+shell.exec(`bash -c 'rsync -am --include="*."{vue,scss,html,md,js,d.ts} --include="*/" --exclude="*" src/ lib/'`);
+shell.find('lib')
+    .filter((file) => file.endsWith('.js'))
+    .forEach((file) => {
+        shell.mv(file, file.replace('.js', '.mjs'));
+    });
 
 /**
  * Replace references in .vue files
  */
 
-shell.exec("bash -c 'find lib -type f -name *.vue -exec perl -i -pe\"s/.ts\\\" lang=\\\"ts\\\"/.mjs\\\"/g\" {} +'");
+shell.exec(`find lib -type f -name *.vue -exec perl -i -pe"s/\\.ts\\" lang=\\"ts\\"/.mjs\\"/g" {} +`);
+shell.exec(`find lib -type f -name *.vue -exec perl -i -pe"s/\\.js\\"/.mjs\\"/g" {} +`);
+
+/**
+ * Resolve sourcemaps
+ */
+
+shell.cp('-R', './src', './lib/src');
+shell.exec(`bash -c 'find lib -type f -name *.mjs.map -exec perl -i -pe"s/..\\/src/src/g" {} +'`);
 
 /**
  * Remove unnecessary files
  */
 
 shell.rm(['./lib/main.*']);
-shell.rm('-R', ['./lib/__storybook__']);
 shell.exec('find lib -name index.stories.* -type f -delete');
+shell.exec('find lib -name __storybook__ -type d -exec rm -rf {} +');
 shell.exec('find lib -name __tests__ -type d -exec rm -rf {} +');
 shell.exec('find lib -name __mocks__ -type d -exec rm -rf {} +');
 
