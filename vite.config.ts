205544e03c4fa443752acf878e99dfd7bc5c8b70
Alex Grozav <alex@grozav.com>
2022-07-01 11:15:00 +0300

    fix: Removed jsdom from dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { configDefaults } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
-        environment: 'jsdom',
         exclude: [...configDefaults.exclude, 'lib/**']
     }
 });
