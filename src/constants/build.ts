f85eaab922f3c9fc9e01231d8031f97deafcfb43
Alex Grozav <alex@grozav.com>
2022-07-28 10:44:34 +0300

    fix: Updated default output dir.

--- src/constants/build.ts
+++ src/constants/build.ts
@@ -1,3 +1,2 @@
-export const DEFAULT_OUTPUT_DIR = '.inkline';
-export const DEFAULT_CSS_OUTPUT_DIR = '.inkline/css';
+export const DEFAULT_OUTPUT_DIR = '.inkline/css';
 export const DEFAULT_OUTPUT_EXTNAME = '.css';
