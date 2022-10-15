98366fc1727bde9cdb3a5cd54b8f4466577d2766
Alex Grozav <alex@grozav.com>
2022-10-15 16:06:19 +0300

    refactor: change default output extname to .scss

--- src/constants/build.ts
+++ src/constants/build.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_CONFIG_FILE = 'inkline.config';
 export const DEFAULT_OUTPUT_DIR = '.inkline/css';
-export const DEFAULT_OUTPUT_EXTNAME = '.css';
+export const DEFAULT_OUTPUT_EXTNAME = '.scss';
 export const DEFAULT_THEME_SELECTOR = '.<% themeName %>-theme';
