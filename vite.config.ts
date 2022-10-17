9117459fc75640dc2b2d737c7c0d9c353d2f592c
Alex Grozav <alex@grozav.com>
2022-10-17 12:21:20 +0300

    fix: update outputDir

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import inspect from 'vite-plugin-inspect';
 import { inkline } from './src/vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         inspect(),
         inkline({
-            outputDir: 'src/playground/css/config'
+            outputDir: resolve(__dirname, 'src/playground/css/config')
         })
     ],
     test: {
