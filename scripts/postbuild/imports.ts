7352338a67915768e2a7d53a592d55316bbe94df
Alex Grozav <alex@grozav.com>
2022-12-26 17:14:24 +0200

    fix: remove unused condition

--- scripts/postbuild/imports.ts
+++ scripts/postbuild/imports.ts
@@ -7,7 +7,9 @@ const rootDir = resolve(__dirname, "..", "..");
 const libDir = resolve(rootDir, "lib");
 
 (async () => {
-    const files = await glob(resolve(libDir, "**/*.{js,mjs,dts,vue}"));
+    const files = await glob(resolve(libDir, "**/*.{js,mjs,dts,vue}"), {
+        ignore: [resolve(libDir, "**", "examples")],
+    });
 
     files.map(async (file) => {
         const contents = await readFile(file, "utf8");
@@ -17,7 +19,6 @@ const libDir = resolve(rootDir, "lib");
             (match, p0, p1) => {
                 const absolutePath = p1.replace(`@inkline/inkline`, libDir);
                 const relativePath = relative(dirname(file), absolutePath);
-                const pathIsDir = existsSync(absolutePath);
 
                 let relativeImportPath = relativePath.startsWith(".")
                     ? relativePath
