2d632d9254b52f5ec1146cc58113e9c7f870dc15
Alex Grozav <alex@grozav.com>
2023-01-30 11:02:48 +0200

    refactor: split up main Inkline plugin into smaller plugins

---
+++ src/plugins/index.ts
@@ -0,0 +1,4 @@
+export * from './color-mode';
+export * from './icons';
+export * from './overlay';
+export * from './toast';
---
+++ src/plugins/toast.ts
@@ -0,0 +1,48 @@
+import { InjectionKey, Plugin, VNode } from "vue";
+import { createEventBus } from "@grozav/utils";
+
+export type ToastPosition = "top-left" | "top" | "top-right" | "right" | "bottom-right" | "bottom" | "bottom-left" | "left";
+
+export interface ToastOptions {
+    id: string;
+    title: string | VNode | VNode[];
+    message: string | VNode | VNode[];
+    position: ToastPosition;
+    duration: number | null;
+    color: 'light' | 'dark' | 'primary' | 'secondary' | 'info' | 'success' | 'warning' | 'danger' | string;
+}
+
+export interface InklineToastOptions {
+    toast?: Partial<Omit<ToastOptions, 'id'>>;
+}
+
+export interface ToastService {
+    show: (options: Partial<ToastOptions>) => void;
+    hide: (id: string) => void;
+    hideAll: () => void;
+}
+
+export const toastEventBus = createEventBus();
+
+export const createToastService = (): ToastService => ({
+    show: (options) => {
+        toastEventBus.emit('show', options);
+    },
+    hide: (id) => {
+        toastEventBus.emit('hide', { id });
+    },
+    hideAll: () => {
+        toastEventBus.emit('hideAll', {});
+    }
+});
+
+export const ToastKey = Symbol('toast') as InjectionKey<ToastService>;
+
+export const ToastPlugin: Plugin = {
+    install: (app) => {
+        const toastService = createToastService();
+
+        app.config.globalProperties.$toast = toastService;
+        app.provide(ToastKey, toastService);
+    }
+};
