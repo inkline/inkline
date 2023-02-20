07091ae4c0c5f9f66d6ba10b270fae551b56231b
Alex Grozav <alex@grozav.com>
2023-02-20 14:57:43 +0200

    fix: add plugins exports in inkline.ts

--- src/inkline.ts
+++ src/inkline.ts
@@ -6,6 +6,7 @@ export * from '@inkline/inkline/constants';
 export * from '@inkline/inkline/controllers';
 export * from '@inkline/inkline/utils';
 export * from '@inkline/inkline/i18n';
+export * from '@inkline/inkline/plugins';
 export * from '@inkline/inkline/plugin';
 export * from '@inkline/inkline/validation';
 export { components };
