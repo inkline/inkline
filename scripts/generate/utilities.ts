57d5916b6c7e2013e7e0d56597d8b5fdeda83387
Alex Grozav <alex@grozav.com>
2023-02-20 11:55:11 +0200

    fix: update generate:utilities script to retrieve prettier config

--- scripts/generate/utilities.ts
+++ scripts/generate/utilities.ts
@@ -10,14 +10,16 @@ const outputFile = path.resolve(cssDir, 'utilities.scss');
 const unocssDir = path.resolve(baseDir, '..', 'unocss');
 const unocssPlaygroundDir = path.resolve(unocssDir, 'src', 'playground');
 const unocssDistDir = path.resolve(unocssPlaygroundDir, 'dist');
+const prettierConfigFile = path.resolve(baseDir, '.prettierrc');
 
 (async () => {
     await shell.exec('npm run build:vite', { cwd: unocssDir });
     const [cssFile] = await glob(path.resolve(unocssDistDir, '**/*.css'));
     const css = await readFile(cssFile, 'utf-8');
+    const prettierConfig = await import(prettierConfigFile);
 
     const code = css.replace(/^[^_]*\._/, '._');
-    const result = prettier.format(code, { parser: 'scss' });
+    const result = prettier.format(code, { parser: 'scss', ...prettierConfig });
 
     await writeFile(outputFile, result);
 })();
