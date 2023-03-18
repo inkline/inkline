7f3f30be86fa4913eb67b660abfc77445ff1b54f
Alex Grozav <alex@grozav.com>
2023-03-18 13:25:35 +0200

    fix: remove playground from release package

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,13 +1,14 @@
-import * as shell from 'shelljs';
-import { resolve } from 'path';
+import * as shell from "shelljs";
+import { resolve } from "path";
 
 /**
  * Change directory to root
  */
 
-shell.cd(resolve(__dirname, '..'));
+shell.cd(resolve(__dirname, ".."));
 
-shell.rm('-r', 'lib/**/__tests__');
+shell.rm("-r", "lib/playground");
+shell.rm("-r", "lib/**/__tests__");
 
-shell.cp('./README.md', 'lib');
-shell.cp('./LICENSE', 'lib');
+shell.cp("./README.md", "lib");
+shell.cp("./LICENSE", "lib");
