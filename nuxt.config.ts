e4add43ad4b1024d0e2f879975879602f0986ffe
Alex Grozav <alex@grozav.com>
2023-05-16 18:05:42 +0300

    fix: fix nuxt dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import { resolve } from 'pathe';
-import { UserOptions } from './src/plugin/types';
+import { resolve } from "pathe";
+import { UserOptions } from "./src/plugin/types";
 
 const inklineConfig: UserOptions = {
-    outputDir: resolve(__dirname, 'src/playground/nuxt/css')
+    outputDir: resolve(__dirname, "src/playground/nuxt/css"),
 };
 
 export default defineNuxtConfig({
-    rootDir: resolve(__dirname, 'src/playground/nuxt'),
+    rootDir: resolve(__dirname, "src/playground/nuxt"),
     devServer: {
-        port: 8080
+        port: 8080,
     },
-    modules: [
-        ['./src/nuxt.ts', inklineConfig]
-    ]
+    modules: [[resolve(__dirname, "./src/nuxt.ts"), inklineConfig]],
 });
