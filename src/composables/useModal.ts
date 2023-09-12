9e98f7b3a6ce6a682b415c215815f7ff12088492
Alex Grozav <alex@grozav.com>
2023-09-12 16:35:39 +0300

    feat: improve useModal type definition

--- src/composables/useModal.ts
+++ src/composables/useModal.ts
@@ -4,8 +4,13 @@ import {
     useModalBuilder,
     usePrompt
 } from '@inkline/inkline/composables/modals';
+import { ModalService } from '@inkline/inkline/plugins';
 
-export function useModal() {
+export function useModal(): ModalService & {
+    alert: ReturnType<typeof useAlert>;
+    confirm: ReturnType<typeof useConfirm>;
+    prompt: ReturnType<typeof usePrompt>;
+} {
     const builder = useModalBuilder();
     const alert = useAlert();
     const confirm = useConfirm();
