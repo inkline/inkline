e5c1da36e540954025ea93230f2fcc87f1ad7e33
Alex Grozav <alex@grozav.com>
2023-10-23 17:55:13 +0300

    feat: add default type to useForm

--- src/composables/useForm.ts
+++ src/composables/useForm.ts
@@ -2,7 +2,7 @@ import { computed, Ref, ref } from 'vue';
 import { createSchema, serializeSchema, validateSchema } from '@inkline/inkline/validation';
 import { Form, FormSchema, ResolvedFormSchema } from '@inkline/inkline/types';
 
-export function useForm<T extends Form>(formSchema: FormSchema<T>) {
+export function useForm<T extends Form = Form>(formSchema: FormSchema<T>) {
     const schema = ref(createSchema<T>(formSchema)) as Ref<ResolvedFormSchema<T>>;
     const form = computed(() => serializeSchema<T>(schema.value));
 
