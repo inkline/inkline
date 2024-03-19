e862aac8a5275dc7d74da11f01731a9e941b3a02
Alex Grozav <alex@grozav.com>
2024-03-19 20:01:22 +0200

    fix: fix build

--- src/__storybook__/index.ts
+++ src/__storybook__/index.ts
@@ -1,3 +1,4 @@
 export * from '@inkline/inkline/__storybook__/argTypes';
 export * from '@inkline/inkline/__storybook__/createStory';
 export * from '@inkline/inkline/__storybook__/createExampleStory';
+export * from '@inkline/inkline/__storybook__/useServer';
--- src/__tests__/utils/index.ts
+++ src/__tests__/utils/index.ts
@@ -1,4 +1,3 @@
 export * from '@inkline/inkline/__tests__/utils/createInkline';
 export * from '@inkline/inkline/__tests__/utils/placeholders';
 export * from '@inkline/inkline/__tests__/utils/retry';
-export * from '@inkline/inkline/__tests__/utils/useServer';
