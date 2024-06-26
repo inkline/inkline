ee899ba1091c86d176d1f758283759d922bd5b40
Alex Grozav <alex@grozav.com>
2024-06-26 19:42:16 +0300

    fix: disabled linting in unocss

--- packages/integrations/unocss/eslint.config.mjs
+++ packages/integrations/unocss/eslint.config.mjs
@@ -4,5 +4,8 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default pluginInkline(__dirname).configs['default'];
+export default [
+    // @TODO Add support for linting
+    // ...pluginInkline(__dirname).configs['default']
+];
 
