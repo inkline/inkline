076e8c8af0ee3253448f8b9a962ebdca2a8c31b4
Alex Grozav <alex@grozav.com>
2024-12-22 20:01:37 +0200

    feat: added one-up spacing variants

--- packages/ui/theme/src/variables/useFontSize.ts
+++ packages/ui/theme/src/variables/useFontSize.ts
@@ -1,4 +1,4 @@
-import { css, ref, TokenValue, variable } from '@inkline/core';
+import { css, multiply, ref, TokenValue, variable } from '@inkline/core';
 import { defaultDefinitionOptions } from '@inkline/core';
 import { useFluid } from './useFluid';
 import { useFluidTypeScale } from './useFluidTypeScale';
@@ -14,16 +14,62 @@ export function useFontSize(options = defaultDefinitionOptions) {
     const fontSizeMin = variable('font-size-min', 18, options);
     const fontSizeMax = variable('font-size-max', 20, options);
 
-    const createFontSizeVariant = (minScale: TokenValue, maxScale: TokenValue) => createVariantFactoryFn(() => css`calc((${ref(fontSizeMin)}${minScale !== 1 ? css` *${' '}${minScale}` : ''} / 16 * ${'1rem'}) + (${ref(fontSizeMax)}${maxScale !== 1 ? css` *${' '}${maxScale}` : ''} - ${ref(fontSizeMin)}${minScale !== 1 ? css` *${' '}${minScale}` : ''}) * ${ref(fluidBreakpoint)})`);
+    const fontSizeMinVariants = {
+        'xs': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['-2']))),
+        'sm': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['-1']))),
+        'md': createVariantFactoryFn((value) => value),
+        'lg': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['1']))),
+        'xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['2']))),
+        '2xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['3']))),
+        '3xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['4']))),
+        '4xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMinMap['5'])))
+    };
+
+    const {
+        fontSizeMinXs,
+        fontSizeMinSm,
+        fontSizeMinMd,
+        fontSizeMinLg,
+        fontSizeMinXl,
+        fontSizeMin2Xl,
+        fontSizeMin3Xl,
+        fontSizeMin4Xl
+    } =
+        useVariantsFactory<'font-size-min', keyof typeof fontSizeMinVariants>(fontSizeMin, fontSizeMinVariants, options);
+
+    const fontSizeMaxVariants = {
+        'xs': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['-2']))),
+        'sm': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['-1']))),
+        'md': createVariantFactoryFn((value) => value),
+        'lg': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['1']))),
+        'xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['2']))),
+        '2xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['3']))),
+        '3xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['4']))),
+        '4xl': createVariantFactoryFn((value) => multiply(value, ref(typeScaleMaxMap['5'])))
+    };
+
+    const {
+        fontSizeMaxXs,
+        fontSizeMaxSm,
+        fontSizeMaxMd,
+        fontSizeMaxLg,
+        fontSizeMaxXl,
+        fontSizeMax2Xl,
+        fontSizeMax3Xl,
+        fontSizeMax4Xl
+    } =
+        useVariantsFactory<'font-size-max', keyof typeof fontSizeMaxVariants>(fontSizeMax, fontSizeMaxVariants, options);
+
+    const createFontSizeVariant = (fontSizeMin: TokenValue, fontSizeMax: TokenValue) => createVariantFactoryFn(() => css`calc(((${fontSizeMin} / 16) * ${'1rem'}) + (${fontSizeMax} - ${fontSizeMin}) * ${ref(fluidBreakpoint)})`);
     const variants = {
-        'xs': createFontSizeVariant(ref(typeScaleMinMap['-2']), ref(typeScaleMaxMap['-2'])),
-        'sm': createFontSizeVariant(ref(typeScaleMinMap['-1']), ref(typeScaleMaxMap['-1'])),
-        'md': createFontSizeVariant(1, 1),
-        'lg': createFontSizeVariant(ref(typeScaleMinMap['1']), ref(typeScaleMaxMap['1'])),
-        'xl': createFontSizeVariant(ref(typeScaleMinMap['2']), ref(typeScaleMaxMap['2'])),
-        '2xl': createFontSizeVariant(ref(typeScaleMinMap['3']), ref(typeScaleMaxMap['3'])),
-        '3xl': createFontSizeVariant(ref(typeScaleMinMap['4']), ref(typeScaleMaxMap['4'])),
-        '4xl': createFontSizeVariant(ref(typeScaleMinMap['5']), ref(typeScaleMaxMap['5']))
+        'xs': createFontSizeVariant(ref(fontSizeMinXs), ref(fontSizeMaxXs)),
+        'sm': createFontSizeVariant(ref(fontSizeMinSm), ref(fontSizeMaxSm)),
+        'md': createFontSizeVariant(ref(fontSizeMinMd), ref(fontSizeMaxMd)),
+        'lg': createFontSizeVariant(ref(fontSizeMinLg), ref(fontSizeMaxLg)),
+        'xl': createFontSizeVariant(ref(fontSizeMinXl), ref(fontSizeMaxXl)),
+        '2xl': createFontSizeVariant(ref(fontSizeMin2Xl), ref(fontSizeMax2Xl)),
+        '3xl': createFontSizeVariant(ref(fontSizeMin3Xl), ref(fontSizeMax3Xl)),
+        '4xl': createFontSizeVariant(ref(fontSizeMin4Xl), ref(fontSizeMax4Xl))
     };
 
     const {
--- packages/ui/theme/src/variables/useSpacing.ts
+++ packages/ui/theme/src/variables/useSpacing.ts
@@ -1,4 +1,4 @@
-import { css, ref, TokenValue, variable } from '@inkline/core';
+import { css, multiply, ref, TokenValue, variable } from '@inkline/core';
 import { defaultDefinitionOptions } from '@inkline/core';
 import { useFluid } from './useFluid';
 import { createVariantFactoryFn, useVariantsFactory } from './useVariantsFactory';
@@ -9,17 +9,74 @@ export function useSpacing(options = defaultDefinitionOptions) {
     const spacingMin = variable('spacing-min', 18, options);
     const spacingMax = variable('spacing-max', 20, options);
 
-    const createSpacingVariant = (value: TokenValue) => createVariantFactoryFn(() => css`calc((${ref(spacingMin)} / 16 * ${'1rem'} + (${ref(spacingMax)} - ${ref(spacingMin)}) * ${ref(fluidBreakpoint)}) ${typeof value === 'number' && value !== 1 ? ` * ${value}` : ''})`);
+    const variantMultipliers = {
+        '2xs': 0.25,
+        'xs': 0.5,
+        'sm': 0.75,
+        'md': 1,
+        'lg': 1.5,
+        'xl': 2,
+        '2xl': 4,
+        '3xl': 6,
+        '4xl': 8
+    };
+
+    const spacingVariants = {
+        '2xs': createVariantFactoryFn((value) => multiply(value, variantMultipliers['2xs'])),
+        'xs': createVariantFactoryFn((value) => multiply(value, variantMultipliers['xs'])),
+        'sm': createVariantFactoryFn((value) => multiply(value, variantMultipliers['sm'])),
+        'md': createVariantFactoryFn((value) => value),
+        'lg': createVariantFactoryFn((value) => multiply(value, variantMultipliers['lg'])),
+        'xl': createVariantFactoryFn((value) => multiply(value, variantMultipliers['xl'])),
+        '2xl': createVariantFactoryFn((value) => multiply(value, variantMultipliers['2xl'])),
+        '3xl': createVariantFactoryFn((value) => multiply(value, variantMultipliers['3xl'])),
+        '4xl': createVariantFactoryFn((value) => multiply(value, variantMultipliers['4xl']))
+    };
+
+    const {
+        spacingMin2Xs,
+        spacingMinXs,
+        spacingMinSm,
+        spacingMinMd,
+        spacingMinLg,
+        spacingMinXl,
+        spacingMin2Xl,
+        spacingMin3Xl,
+        spacingMin4Xl
+    } = useVariantsFactory<'spacing-min', keyof typeof spacingVariants>(spacingMin, spacingVariants, options);
+
+
+    const {
+        spacingMax2Xs,
+        spacingMaxXs,
+        spacingMaxSm,
+        spacingMaxMd,
+        spacingMaxLg,
+        spacingMaxXl,
+        spacingMax2Xl,
+        spacingMax3Xl,
+        spacingMax4Xl
+    } = useVariantsFactory<'spacing-max', keyof typeof spacingVariants>(spacingMax, spacingVariants, options);
+
+    const createSpacingVariant = (spacingMin: TokenValue, spacingMax: TokenValue) => createVariantFactoryFn(() => css`calc(((${spacingMin} / 16) * 1 rem) + (${spacingMax} - ${spacingMin}) * ${ref(fluidBreakpoint)})`);
     const variants = {
-        '2xs': createSpacingVariant(0.25),
-        'xs': createSpacingVariant(0.5),
-        'sm': createSpacingVariant(0.75),
-        'md': createSpacingVariant(1),
-        'lg': createSpacingVariant(1.5),
-        'xl': createSpacingVariant(2),
-        '2xl': createSpacingVariant(4),
-        '3xl': createSpacingVariant(6),
-        '4xl': createSpacingVariant(8)
+        '2xs': createSpacingVariant(spacingMin2Xs, spacingMax2Xs),
+        'xs': createSpacingVariant(spacingMinXs, spacingMaxXs),
+        'sm': createSpacingVariant(spacingMinSm, spacingMaxSm),
+        'md': createSpacingVariant(spacingMinMd, spacingMaxMd),
+        'lg': createSpacingVariant(spacingMinLg, spacingMaxLg),
+        'xl': createSpacingVariant(spacingMinXl, spacingMaxXl),
+        '2xl': createSpacingVariant(spacingMin2Xl, spacingMax2Xl),
+        '3xl': createSpacingVariant(spacingMin3Xl, spacingMax3Xl),
+        '4xl': createSpacingVariant(spacingMin4Xl, spacingMax4Xl),
+        '2xs-xs': createSpacingVariant(spacingMin2Xs, spacingMaxXs),
+        'xs-sm': createSpacingVariant(spacingMinXs, spacingMaxSm),
+        'sm-md': createSpacingVariant(spacingMinSm, spacingMaxMd),
+        'md-lg': createSpacingVariant(spacingMinMd, spacingMaxLg),
+        'lg-xl': createSpacingVariant(spacingMinLg, spacingMaxXl),
+        'xl-2xl': createSpacingVariant(spacingMinXl, spacingMax2Xl),
+        '2xl-3xl': createSpacingVariant(spacingMin2Xl, spacingMax3Xl),
+        '3xl-4xl': createSpacingVariant(spacingMin3Xl, spacingMax4Xl)
     };
 
     const {
@@ -31,7 +88,15 @@ export function useSpacing(options = defaultDefinitionOptions) {
         spacingXl,
         spacing2Xl,
         spacing3Xl,
-        spacing4Xl
+        spacing4Xl,
+        spacing2XsXs,
+        spacingXsSm,
+        spacingSmMd,
+        spacingMdLg,
+        spacingLgXl,
+        spacingXl2Xl,
+        spacing2Xl3Xl,
+        spacing3Xl4Xl
     } =
         useVariantsFactory<'spacing', keyof typeof variants>('spacing', variants, options);
 
@@ -49,6 +114,14 @@ export function useSpacing(options = defaultDefinitionOptions) {
         spacingXl,
         spacing2Xl,
         spacing3Xl,
-        spacing4Xl
+        spacing4Xl,
+        spacing2XsXs,
+        spacingXsSm,
+        spacingSmMd,
+        spacingMdLg,
+        spacingLgXl,
+        spacingXl2Xl,
+        spacing2Xl3Xl,
+        spacing3Xl4Xl
     };
 }
